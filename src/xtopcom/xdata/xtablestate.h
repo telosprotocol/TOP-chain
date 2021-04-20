f0f7015d5b4394e8cd49b57501c735595a17a673
wishwen <wish.wen@topnetwork.org>
2021-04-20 08:54:47 +0800

    feat(grpc): add interface getLatestFullBlock

--- src/xtopcom/xdata/xtablestate.h
+++ src/xtopcom/xdata/xtablestate.h
@@ -53,6 +53,7 @@ class xtablestate_t : public base::xdataunit_t {
  public:  // apis for user query
     void        set_account_index(const std::string & account, const base::xaccount_index_t & account_index);
     bool        get_account_index(const std::string & account, base::xaccount_index_t & account_index);
+    uint64_t    get_account_size() const { return m_accountindex_state->get_account_size(); }
     bool        find_receiptid_pair(base::xtable_shortid_t sid, base::xreceiptid_pair_t & pair);
 
  protected:
