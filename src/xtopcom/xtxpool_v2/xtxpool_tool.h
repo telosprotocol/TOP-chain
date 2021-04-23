f474c9c644d7821e4461c0940287ae4302d8ae24
nathan <nathan.sun@topnetwork.org>
2021-04-23 17:50:56 +0800

    fix(txpool unitservice):TOP-3623,TOP-3622

--- src/xtopcom/xtxpool_v2/xtxpool_tool.h
+++ src/xtopcom/xtxpool_v2/xtxpool_tool.h
@@ -20,6 +20,8 @@ using xpeer_table_receipt_map_t = std::unordered_map<base::xtable_shortid_t, std
 class xordered_ready_txs_t {
 public:
     xordered_ready_txs_t(ready_accounts_t reday_accounts);
+    void erase_noncontinuous_receipts(const base::xreceiptid_state_ptr_t receiptid_state);
+    ready_accounts_t get_ready_accounts() const;
     bool is_receipts_id_state_continuous(const base::xreceiptid_state_ptr_t receiptid_state);
     static ready_accounts_t ready_txs_to_ready_accounts(std::vector<xcons_transaction_ptr_t> ready_txs);
 
@@ -27,6 +29,7 @@ private:
     void add_tx(const xcons_transaction_ptr_t & tx, xpeer_table_receipt_map_t & peer_table_map);
     xpeer_table_receipt_map_t m_peer_table_recv_tx_map;
     xpeer_table_receipt_map_t m_peer_table_confirm_tx_map;
+    std::vector<xcons_transaction_ptr_t> m_send_txs;
 };
 
 
