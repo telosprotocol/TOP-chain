c9ca4eeb4405024783f0d344bada470cbda3c435
nathan <nathan.sun@topnetwork.org>
2021-04-28 14:41:10 +0800

    fix(txpool):remove tx filter, optimaze unconfirm txs treatment

--- src/xtopcom/xtxpool_v2/xtxpool_face.h
+++ src/xtopcom/xtxpool_v2/xtxpool_face.h
@@ -195,7 +195,7 @@ public:
     virtual const std::vector<xcons_transaction_ptr_t> get_resend_txs(uint8_t zone, uint16_t subaddr, uint64_t now) = 0;
     virtual void update_unconfirm_accounts(uint8_t zone, uint16_t subaddr) = 0;
     virtual void update_non_ready_accounts(uint8_t zone, uint16_t subaddr) = 0;
-    virtual void update_locked_txs(const std::string & table_addr, const std::vector<tx_info_t> & locked_tx_vec) = 0;
+    virtual void update_locked_txs(const std::string & table_addr, const std::vector<tx_info_t> & locked_tx_vec, const base::xreceiptid_state_ptr_t & receiptid_state) = 0;
     virtual void update_receiptid_state(const std::string & table_addr, const base::xreceiptid_state_ptr_t & receiptid_state) = 0;
 };
 
