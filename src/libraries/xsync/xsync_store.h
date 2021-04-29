05bc97e49905b1bde74d51f68f3cdd7acc1f9007
forme628 <forme.he@topnetwork.org>
2021-04-29 11:55:00 +0800

    fest(sync):unit sync by hash

--- src/libraries/xsync/xsync_store.h
+++ src/libraries/xsync/xsync_store.h
@@ -29,6 +29,7 @@ public:
     virtual base::xauto_ptr<base::xvblock_t> get_latest_start_block(const std::string & account, enum_chain_sync_policy sync_policy) = 0;
     virtual base::xauto_ptr<base::xvblock_t> get_latest_end_block(const std::string & account, enum_chain_sync_policy sync_policy) = 0;
     virtual std::vector<data::xvblock_ptr_t> load_block_objects(const std::string & account, const uint64_t height) = 0;
+    virtual std::vector<data::xvblock_ptr_t> load_block_objects(const std::string & tx_hash, const base::enum_transaction_subtype type) = 0;
     const static uint64_t m_undeterministic_heights = 2;
 };
 
@@ -46,6 +47,7 @@ public:
     virtual base::xauto_ptr<base::xvblock_t> get_latest_start_block(const std::string & account, enum_chain_sync_policy sync_policy) {return nullptr;}
     virtual base::xauto_ptr<base::xvblock_t> get_latest_end_block(const std::string & account, enum_chain_sync_policy sync_policy) {return nullptr;}
     virtual std::vector<data::xvblock_ptr_t> load_block_objects(const std::string & account, const uint64_t height) {return std::vector<data::xvblock_ptr_t>{};}
+    virtual std::vector<data::xvblock_ptr_t> load_block_objects(const std::string & tx_hash, const base::enum_transaction_subtype type) {return std::vector<data::xvblock_ptr_t>{};}
 };
 
 class xsync_store_t : public xsync_store_face_t {
@@ -63,6 +65,7 @@ public:
     base::xauto_ptr<base::xvblock_t> get_latest_start_block(const std::string & account, enum_chain_sync_policy sync_policy) override;
     base::xauto_ptr<base::xvblock_t> get_latest_end_block(const std::string & account, enum_chain_sync_policy sync_policy) override;
     std::vector<data::xvblock_ptr_t> load_block_objects(const std::string & account, const uint64_t height) override;
+    std::vector<data::xvblock_ptr_t> load_block_objects(const std::string & tx_hash, const base::enum_transaction_subtype type) override;
 
 private:
     std::string m_vnode_id;
