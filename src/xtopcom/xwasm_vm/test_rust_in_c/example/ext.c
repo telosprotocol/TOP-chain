0fd6979d5cc4e3d3ee1cd183c84e13b5da20b598
Charles.Liu <charles.liu@topnetwork.org>
2021-04-30 13:15:15 +0800

    dev(erc20): compile rustvm into xchain

--- src/xtopcom/xwasm_vm/test_rust_in_c/example/ext.c
+++
@@ -1,6 +0,0 @@
-extern int fib(int n);
-extern void print(int n);
-int cal(n) {
-    print(n);
-    return fib(n);
-}
--- src/xtopcom/xwasm_vm/test_rust_in_c/example/test_main.c
+++
@@ -1,9 +0,0 @@
-extern int test_import(int k);
-extern int test_import2(int k, int i);
-
-int main() {
-    int a = 1, b = 2;
-    test_import(a);
-    test_import2(a, b);
-    return a + b;
-}
