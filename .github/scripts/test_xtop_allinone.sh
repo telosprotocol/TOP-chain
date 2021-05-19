378beb1444a64c79f6fb42904964fd6358af18d8
aries <aries.zhang@topnetwork.org>
2021-05-19 15:58:11 +0800

    more info

--- .github/scripts/test_xtop_allinone.sh
+++ .github/scripts/test_xtop_allinone.sh
@@ -50,24 +50,36 @@ echo "hit log: "${ret}
 echo "====== test tx ======"
 ./topio wallet setDefaultAccount T00000Lhj29VReFAT958ZqFWZ2ZdMLot2PS5D5YC
 sleep 1
-ret=$(./topio wallet listaccounts|grep T00000Lhj29VReFAT958ZqFWZ2ZdMLot2PS5D5YC -A 3|grep balance)
-if [[ ${ret} != "balance: 2999997000.000000 TOP" ]];then
-    echo "check god fail: "${ret}
+accounts_info=$(./topio wallet listaccounts)
+balance=$(echo "${accounts_info}"|grep T00000Lhj29VReFAT958ZqFWZ2ZdMLot2PS5D5YC -A 3|grep balance)
+if [[ ${balance} != "balance: 2999997000.000000 TOP" ]];then
+    echo "check god balance fail, see follow output:"
+    echo "${accounts_info}"
     sh ${clear} -o archive -i ${NUM} -d ${workdir}
     exit -1
 fi
 addr=$(./topio wallet createaccount | grep -a "T00000"|awk -F ':' '{print $2}')
 echo "new local addr: "${addr}
-tx=$(./topio transfer ${addr} 123456 tx_test | grep -a "Transaction hash"|awk -F ':' '{print $2}')
+tx_info=$(./topio transfer ${addr} 123456 tx_test)
+tx=$(echo "${tx_info}" | grep -a "Transaction hash"|awk -F ':' '{print $2}')
+if [[ -z ${tx} ]];then
+    echo "tx fail, see follow output:"
+    echo "${tx_info}"
+    sh ${clear} -o archive -i ${NUM} -d ${workdir}
+    exit -1
+fi
 echo "tx: "${tx}
 sleep 60
-stat=$(./topio querytx ${tx} | grep -a '\"exec_status\"'|grep "success"|wc -l)
+tx_ret=$(./topio querytx ${tx})
+stat=$(echo "${tx_ret}" | grep -a '\"exec_status\"'|grep "success"|wc -l)
 if [[ ${stat} -eq 1 ]];then
     echo "====== tx success, end ======"
     sh ${clear} -o clean
 else
-    echo "====== tx fail, end ======"
+    echo "tx check fail, see follow output:"
+    echo "${tx_ret}"
     sh ${clear} -o archive -i ${NUM} -d ${workdir}
+    echo "====== tx fail, end ======"
     exit -1
 fi
 
