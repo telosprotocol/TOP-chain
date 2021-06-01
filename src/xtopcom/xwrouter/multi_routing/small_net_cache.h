864263c9cb245a34cbea60b50e5220a069ad3178
Charles.Liu <charles.liu@topnetwork.org>
2021-06-01 11:42:37 +0800

    perf(p2p): delete unused members&&functions in small_net_cache

--- src/xtopcom/xwrouter/multi_routing/small_net_cache.h
+++ src/xtopcom/xwrouter/multi_routing/small_net_cache.h
@@ -31,30 +31,18 @@ public:
     bool Init();
 
     void GetAllServiceType(std::set<uint64_t> & svec);
-
     uint32_t AddNode(NetNode node);
-
     bool FindNewNode(NetNode & Fnode, uint64_t service_type);
     bool FindRandomNode(NetNode & Fnode, uint64_t service_type);
     bool FindAllNode(std::vector<NetNode> & node_vec, uint64_t service_type);
-    void GetAllNodes(std::vector<NetNode> & node_vec);
-
     void GetAllNode(std::vector<NetNode> & node_vec);
-    bool AllowAdd(const std::string & node_id);  // allow add node from routing table
-    bool CheckHasNode(const std::string & node_id, uint64_t service_type);
-    bool CheckMarkExpired(const std::string & node_id);  // latest 10 mins mark expired nodes
-    void do_clear_and_reset();
-    // clear mark expired vector
-    void do_clear_for_mark_expired();
-    bool HasServiceType(const uint64_t & service_type);
 
 private:
     SmallNetNodes();
     ~SmallNetNodes();
     void AddNodeLimit(uint64_t service_type, std::deque<NetNode> & nodes, const NetNode & node);
-    void HandleExpired(std::unordered_map<uint64_t, std::vector<std::string>> & expired_vec,
-                       std::vector<uint64_t> & unreg_service_type_vec,
-                       std::vector<MarkExpiredNetNode> & mark_expired_netnode_vec);
+    void HandleExpired(std::unordered_map<uint64_t, std::vector<std::string>> & expired_vec, std::vector<uint64_t> & unreg_service_type_vec);
+    void do_clear_and_reset();
 
 private:
     std::mutex net_nodes_cache_map_mutex_;
@@ -64,13 +52,9 @@ private:
         std::deque<NetNode> nodes;
     };
     std::unordered_map<uint64_t, std::shared_ptr<NetNodes>> net_nodes_cache_map_;
-    std::mutex local_node_cache_map_mutex_;
-    // keep local node info(xip,account,public_key...)
-    std::map<uint64_t, NetNode> local_node_cache_map_;
+
     std::shared_ptr<base::TimerRepeated> clear_timer_{nullptr};
-    std::shared_ptr<base::TimerRepeated> clear_markexpired_timer_{nullptr};
-    std::mutex mark_expired_netnode_vec_mutex_;
-    std::vector<MarkExpiredNetNode> mark_expired_netnode_vec_;
+
     ServiceNodes * service_nodes_{nullptr};
 };
 
