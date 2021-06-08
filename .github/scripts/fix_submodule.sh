3e129bf1c186ffc92c264c6973b3ae33618fbb14
aries <aries.zhang@topnetwork.org>
2021-06-08 09:34:47 +0800

    clean submodule fetch cmd

--- .github/scripts/fix_submodule.sh
+++ .github/scripts/fix_submodule.sh
@@ -2,15 +2,15 @@
 source /etc/profile
 set -x
 
-BRANCH=$1
+# BRANCH=$1
 
 git submodule update --init --recursive
 
-BASE_DIR=$(pwd)
-echo ${BASE_DIR}
-cd src/xtopcom/xvm && git fetch && git checkout ${BRANCH} && git pull
-cd ${BASE_DIR}
-cd src/xtopcom/xbase && git fetch && git checkout ${BRANCH} && git pull
-cd ${BASE_DIR}
+# BASE_DIR=$(pwd)
+# echo ${BASE_DIR}
+# cd src/xtopcom/xvm && git fetch && git checkout ${BRANCH} && git pull
+# cd ${BASE_DIR}
+# cd src/xtopcom/xbase && git fetch && git checkout ${BRANCH} && git pull
+# cd ${BASE_DIR}
 
 echo "finish submodule"
