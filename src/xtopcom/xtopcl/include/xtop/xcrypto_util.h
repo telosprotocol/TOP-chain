15e691a806114ab28b0e06e323f1c8d77c9b4055
Hench <hench@HenchdeMac-mini.local>
2021-06-22 14:34:58 +0800

    merge taylor version xckey

--- src/xtopcom/xtopcl/include/xtop/xcrypto_util.h
+++ src/xtopcom/xtopcl/include/xtop/xcrypto_util.h
@@ -22,12 +22,6 @@ public:
                                                           uint16_t ledger_id = 0);
     static std::string make_address_by_random_key(uint8_t addr_type = 0, uint16_t ledger_id = 0);
 
-    static std::string make_eth_address_by_assigned_key(std::array<uint8_t, PRI_KEY_LEN> & private_key, uint16_t ledger_id = 0);
-    static std::string make_child_eth_address_by_assigned_key(const std::string & parent_addr,
-                                                          std::array<uint8_t, PRI_KEY_LEN> & private_key,
-                                                          uint16_t ledger_id = 0);
-    static std::string make_eth_address_by_random_key(uint16_t ledger_id = 0);
-
     static std::string get_base64_public_key(const std::array<uint8_t, PRI_KEY_LEN> & private_key);
     static std::string get_hex_public_key(const std::array<uint8_t, PRI_KEY_LEN> & private_key);    
     static std::string digest_sign(const top::uint256_t & hash, const std::array<uint8_t, PRI_KEY_LEN> & private_key);
