09f0abbe94174c3ff5b58fb484b2c9ca33edcdaa
taylor wei <taylor.wei@topnetwork.org>
2021-06-23 17:06:46 -0700

    no message

--- src/xtopcom/xBFT/src/xconsdriver.cpp
+++ src/xtopcom/xBFT/src/xconsdriver.cpp
@@ -891,7 +891,7 @@ namespace top
 
                     _to_remove->release_ref();
                 }
-                timeout_list.clear();
+                outofdate_list.clear();
             }
             return true;
         }
