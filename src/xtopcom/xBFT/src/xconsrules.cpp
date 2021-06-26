9787bc5abe991eb867f641c63df9126aec671cd4
yarkin <zql0114@gmail.com>
2021-06-26 10:54:10 +0800

    chore(bft): update marco

--- src/xtopcom/xBFT/src/xconsrules.cpp
+++ src/xtopcom/xBFT/src/xconsrules.cpp
@@ -787,8 +787,10 @@ namespace top
             {
                 if(_test_for_block->get_justify_cert_hash() != get_lock_block()->get_input_root_hash())
                 {
+#if defined(ENABLE_METRICS)
                     auto fork_tag = "cons_lock_fork_" + get_account();
                     XMETRICS_COUNTER_INCREMENT( fork_tag , 1);
+#endif
                     xerror("xBFTRules::safe_check_follow_locked_branch,fail-proposal justify cert hash unmatch of prev->prev, proposal=%s vs locked=%s at node=0x%llx",_test_for_block->dump().c_str(), get_lock_block()->dump().c_str(),get_xip2_addr().low_addr);
                     return -1;
                 }
