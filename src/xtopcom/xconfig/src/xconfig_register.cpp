3e88b779b3923be1cd27b49bb39e8568341fcd70
wens.weng <wens.wq@gmail.com>
2021-07-07 16:42:30 +0800

    perf&style(contract): refactor & add table split

--- src/xtopcom/xconfig/src/xconfig_register.cpp
+++ src/xtopcom/xconfig/src/xconfig_register.cpp
@@ -119,6 +119,7 @@ void xconfig_register_t::init_static_config() {
     XADD_OFFCHAIN_PARAMETER(local_whitelist);
 
     XADD_OFFCHAIN_PARAMETER(slash_fulltable_interval);
+    XADD_OFFCHAIN_PARAMETER(slash_table_split_num);
 
     m_param_lock.release_write();
 }
