490674eef0700f959a93a5ac82641af5b35025bc
Charles.Liu <charles.liu@topnetwork.org>
2021-08-02 09:54:48 +0800

    perf(metrics): add XMETRICS_ARRCNT_

--- tests/xmetrics/test_metrics.cpp
+++ tests/xmetrics/test_metrics.cpp
@@ -16,10 +16,10 @@
 class metrics_test : public testing::Test {
 public:
     void SetUp() {
-        top::metrics::e_metrics::get_instance().m_metrics_hub.clear();
-        top::metrics::e_metrics::get_instance().start();
         std::size_t test_dump_interval{2};
         XMETRICS_CONFIG_SET("dump_interval", test_dump_interval);
+        top::metrics::e_metrics::get_instance().m_metrics_hub.clear();
+        top::metrics::e_metrics::get_instance().start();
     }
     void TearDown() {
         SLEEP_SECOND(3);
@@ -32,7 +32,7 @@ TEST(test_metrics, config) {
     // config_center.set("string",value);
     std::size_t get_dump_interval{};
     config_center.get("dump_interval", get_dump_interval);
-    ASSERT_EQ(get_dump_interval, 180);
+    ASSERT_EQ(get_dump_interval, 300);
     get_dump_interval = 360;
     config_center.set("dump_interval", get_dump_interval);
     std::size_t new_dump_interval;
@@ -50,21 +50,75 @@ TEST(test_metrics, config) {
 }
 
 TEST_F(metrics_test, basic_function) {
+    SLEEP_SECOND(1);
     std::size_t test_basic_size = 1000;
     for (std::size_t index = 0; index < test_basic_size; ++index) {
+        XMETRICS_ARRCNT_SET(top::metrics::xmetrics_array_tag_t::test_metrics1, rand() % 64, 999);
         XMETRICS_COUNTER_INCREMENT("test_count_metrics1", static_cast<uint64_t>((100 + rand()) % 10000));
         XMETRICS_COUNTER_INCREMENT("test_count_metrics2", static_cast<uint64_t>((100 + rand()) % 10000));
         SLEEP_NANOSECOND(rand() % 10);
-        // }
-        // for (std::size_t index = 0; index < test_basic_size; ++index) {
+        XMETRICS_ARRCNT_INCR(top::metrics::xmetrics_array_tag_t::test_metrics1, rand() % 64, 100);
         XMETRICS_TIME_RECORD("test_time_metrics");
+
         SLEEP_NANOSECOND(rand() % 10);
-        // }
-        // for (std::size_t index = 0; index < test_basic_size; ++index) {
+        XMETRICS_ARRCNT_DECR(top::metrics::xmetrics_array_tag_t::test_metrics1, rand() % 64, 100);
         XMETRICS_FLOW_COUNT("test_flow_metrics1", static_cast<uint64_t>(100));
         XMETRICS_FLOW_COUNT("test_flow_metrics2", static_cast<uint64_t>((100 + rand()) % 10000));
         SLEEP_NANOSECOND(rand() % 10);
     }
+}
+
+std::vector<std::pair<std::size_t, int64_t>> generate_random_data(std::size_t range, std::size_t num) {
+    std::vector<std::pair<std::size_t, int64_t>> res;
+    for (std::size_t i = 0; i < num; ++i) {
+        res.push_back({rand() % range, rand() % 1000});
+    }
+    return res;
+}
+
+void test_metrics_array_counter_thread(top::metrics::xmetrics_array_tag_t tag,std::vector<std::pair<std::size_t,int64_t>> const & data){
+    for(auto const & _p : data){
+        XMETRICS_ARRCNT_INCR(tag,_p.first,_p.second);
+        SLEEP_MILLSECOND(rand() % 10);
+    }
+}
+
+void calc_expect_res(std::vector<std::pair<uint64_t,int64_t>> & res,std::vector<std::pair<std::size_t,int64_t>> const & data){
+    for (auto const & _p : data) {
+        res[_p.first].first += 1;
+        res[_p.first].second += _p.second;
+    }
+}
+
+TEST_F(metrics_test,array_multi_thread){
+    auto data1 = generate_random_data(64,1000);
+    auto data2 = generate_random_data(64,1000);
+    auto data3 = generate_random_data(64,1000);
+    std::thread t1 = std::thread(&test_metrics_array_counter_thread,top::metrics::xmetrics_array_tag_t::test_metrics2,data1);
+    t1.detach();
+    std::thread t2 = std::thread(&test_metrics_array_counter_thread,top::metrics::xmetrics_array_tag_t::test_metrics2,data2);
+    t2.detach();
+    std::thread t3 = std::thread(&test_metrics_array_counter_thread,top::metrics::xmetrics_array_tag_t::test_metrics2,data3);
+    t3.detach();
+
+    std::vector<std::pair<uint64_t,int64_t>> exp_res(64,{0,0});
+    calc_expect_res(exp_res,data1);
+    calc_expect_res(exp_res,data2);
+    calc_expect_res(exp_res,data3);
+    printf("each_value:[");
+    for (auto const & _p : exp_res) {
+        printf("%ld,",_p.second);
+    }
+    printf("]\n");
+    printf("each_count:[");
+    for (auto const & _p : exp_res) {
+        printf("%ld,",_p.first);
+    }
+    printf("]\n");
+
+    SLEEP_SECOND(2);
+    XMETRICS_COUNTER_INCREMENT("drives", 1);
+    SLEEP_SECOND(3);
 }
 
 TEST_F(metrics_test, test_timed_out) {
