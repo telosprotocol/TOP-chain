b6a0b245d3db2313120d5af8cf97b47f6e30253c
Hench <hench.ye@topnetwork.org>
2021-08-10 17:22:59 +0800

    add address unit test

--- src/xtopcom/xverifier/src/xverifier_utl.cpp
+++ src/xtopcom/xverifier/src/xverifier_utl.cpp
@@ -6,7 +6,6 @@
 #include "xdata/xgenesis_data.h"
 #include "xverifier/xverifier_utl.h"
 #include "xbase/xutl.h"
-#include "xrpc/xuint_format.h"
 
 NS_BEG2(top, xverifier)
 
@@ -34,6 +33,12 @@ int32_t xtx_utl::address_is_valid(const std::string & addr) {
         xwarn("[global_trace][xverifier][address_is_valid][fail] address: %s", addr.c_str());
         return xverifier_error::xverifier_error_addr_invalid;
     }
+
+    if (addr[0] != 'T') {
+            xwarn("[global_trace][verifier][address_is_valid]addr invalid: %s", addr.c_str());
+            return  xverifier_error::xverifier_error_addr_invalid;
+    }
+
     base::enum_vaccount_addr_type addr_type = base::xvaccount_t::get_addrtype_from_account(addr);
     std::string addrtemp(addr);
     if (addr_type == top::base::enum_vaccount_addr_type_secp256k1_eth_user_account
@@ -45,10 +50,6 @@ int32_t xtx_utl::address_is_valid(const std::string & addr) {
             xwarn("[global_trace][verifier][address_is_valid]addr invalid: %s", addr.c_str());
             return  xverifier_error::xverifier_error_addr_invalid;
         }
-    }
-    if (addr[0] != 'T') {
-            xwarn("[global_trace][verifier][address_is_valid]addr invalid: %s", addr.c_str());
-            return  xverifier_error::xverifier_error_addr_invalid;
     }
 
     xdbg("[global_trace][xverifier][address_is_valid][success] address: %s", addr.c_str());
@@ -132,7 +133,7 @@ int32_t  xtx_utl::judge_normal_contract_sendtx(data::xtransaction_ptr_t const& t
 }
 bool xtx_utl::is_valid_hex_format(std::string const& str) {
     for (size_t i = 0; i < str.size(); ++i) {
-        if (-1 == xrpc::hex_to_dec(str[i])) {
+        if (-1 == top::data::xaction_t::hex_to_dec(str[i])) {
             return false;
         }
     }
