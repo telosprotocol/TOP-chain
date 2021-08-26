8e26a49250a233c4f27cf370a317c1900092ae3c
wens.weng <wens.wq@gmail.com>
2021-08-26 19:28:49 +0800

    feat(contract): update framework

--- src/xtopcom/xcontract_runtime/xsystem/xsystem_action_runtime.h
+++ src/xtopcom/xcontract_runtime/xsystem/xsystem_action_runtime.h
@@ -10,7 +10,7 @@
 NS_BEG2(top, contract_runtime)
 
 template <>
-class xtop_action_runtime<data::xconsensus_action_t<data::xtop_action_type_t::user>> {
+class xtop_action_runtime<data::xconsensus_action_t<data::xtop_action_type_t::system>> {
 public:
     xtop_action_runtime() = default;
     xtop_action_runtime(xtop_action_runtime const &) = delete;
@@ -19,13 +19,13 @@ public:
     xtop_action_runtime & operator=(xtop_action_runtime &&) = default;
     ~xtop_action_runtime() = default;
 
-    std::unique_ptr<xaction_session_t<data::xconsensus_action_t<data::xtop_action_type_t::user>>> new_session(observer_ptr<contract_common::xcontract_state_t> contract_state);
+    std::unique_ptr<xaction_session_t<data::xconsensus_action_t<data::xtop_action_type_t::system>>> new_session(observer_ptr<contract_common::xcontract_state_t> contract_state);
 
     xtransaction_execution_result_t execute(observer_ptr<contract_common::xcontract_execution_context_t> tx_ctx);
 };
 
 NS_END2
 
-NS_BEG3(top, contract_runtime, user)
-using xuser_action_runtime_t = xtop_action_runtime<data::xconsensus_action_t<data::xtop_action_type_t::user>>;
+NS_BEG3(top, contract_runtime, system)
+using xsystem_action_runtime_t = xtop_action_runtime<data::xconsensus_action_t<data::xtop_action_type_t::system>>;
 NS_END3
