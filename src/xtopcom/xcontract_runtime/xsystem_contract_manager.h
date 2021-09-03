261f1d46584ea1b6e59e72aaf3a234949a7c0070
LonDong <lon.dong@topnetwork.org>
2021-09-03 19:02:57 +0800

    merge and add contract manager interface

--- src/xtopcom/xcontract_runtime/xsystem_contract_manager.h
+++ src/xtopcom/xcontract_runtime/xsystem_contract_manager.h
@@ -27,23 +27,22 @@ enum class enum_contract_broadcast_policy: uint8_t {
 };
 using contract_broadcast_policy_t = enum_contract_broadcast_policy;
 
-
 class xtop_system_contract_manager {
     struct xtop_contract_deployment_data {
         std::shared_ptr<system_contracts::xbasic_system_contract_t> m_system_contract;
         xblock_sniff_config_t m_sniff_config;
         contract_deploy_type_t m_deploy_type;
-        common::xnode_type_t m_broadcast_target{ common::xnode_type_t::invalid };
-        contract_broadcast_policy_t m_broadcast_policy{ contract_broadcast_policy_t::invalid };
+        common::xnode_type_t m_broadcast_target{common::xnode_type_t::invalid};
+        contract_broadcast_policy_t m_broadcast_policy{contract_broadcast_policy_t::invalid};
 
         xtop_contract_deployment_data(std::shared_ptr<system_contracts::xbasic_system_contract_t> contract, xblock_sniff_config_t config)
-          : system_contract(contract), sniff_config(std::move(config)) {
+          : m_system_contract(contract), m_sniff_config(std::move(config)) {
         }
     };
     using xcontract_deployment_data_t = xtop_contract_deployment_data;
 
     std::unordered_map<common::xaccount_address_t, xcontract_deployment_data_t> m_system_contract_deployment_data;
-    base::xvblockstore_t* m_blockstore;
+    // base::xvblockstore_t* m_blockstore;
 
 public:
     xtop_system_contract_manager() = default;
@@ -53,30 +52,35 @@ public:
     xtop_system_contract_manager & operator=(xtop_system_contract_manager &&) = default;
     ~xtop_system_contract_manager() = default;
 
-
     /**
      * @brief get an instance
      *
      * @return xtop_system_contract_manager&
      */
-    static xtop_system_contract_manager& instance();
-    void initialize(base::xvblockstore_t* blockstore);
+    static xtop_system_contract_manager & instance();
     void deploy();
-
-    observer_ptr<system_contracts::xbasic_system_contract_t> system_contract(common::xaccount_address_t const & address) const noexcept;
-
+    void setup(base::xvblockstore_t * blockstore);
 private:
-    void deploy_system_contract(common::xaccount_address_t const& address,
-                                xblock_sniff_config_t sniff_config,
-                                contract_deploy_type_t deploy_type,
-                                std::string const& broadcast_target,
-                                contract_broadcast_policy_t broadcast_policy = contract_broadcast_policy_t::invalid);
-
-    void init_system_contract(common::xaccount_address_t const & contract_address);
-
+    observer_ptr<system_contracts::xbasic_system_contract_t> system_contract(common::xaccount_address_t const & address) const noexcept;
     bool contains(common::xaccount_address_t const & address) const noexcept;
 
-    bool process_sniff(base::xvblock_ptr_t block);
+private:
+    // void deploy_system_contract(common::xaccount_address_t const& address,
+    //                             xblock_sniff_config_t sniff_config,
+    //                             contract_deploy_type_t deploy_type,
+    //                             std::string const& broadcast_target,
+    //                             contract_broadcast_policy_t broadcast_policy = contract_broadcast_policy_t::invalid);
+
+    // void init_system_contract(common::xaccount_address_t const & contract_address);
+
+    void setup_address(const common::xaccount_address_t & contract_address, const common::xaccount_address_t & cluster_address);
+    // need vm support
+    void setup_chain(const common::xaccount_address_t & cluster_address, base::xvblockstore_t * blockstore);
+    bool process_sniffed(const base::xvblock_ptr_t & block);
+    // bool do_on_timer(const common::xaccount_address_t & contract, const xblock_sniff_config_data_t & data, const base::xvblock_ptr_t & block);
+
+    std::unordered_map<common::xaccount_address_t, observer_ptr<system_contracts::xbasic_system_contract_t>> m_contract_inst_map;
+    base::xrwlock_t m_rwlock;
 };
 using xsystem_contract_manager_t = xtop_system_contract_manager;
 
--- src/xtopcom/xsystem_contracts/xbasic_system_contract.h
+++ src/xtopcom/xsystem_contracts/xbasic_system_contract.h
@@ -9,6 +9,10 @@ class xtop_basic_system_contract : public contract_common::xbasic_contract_t {
 public:
     xtop_basic_system_contract(observer_ptr<contract_common::xcontract_execution_context_t> const& exec_context):
                                 contract_common::xbasic_contract_t(exec_context){}
+
+protected:
+    xtop_basic_system_contract() = default;
+    ~xtop_basic_system_contract() override = default;                                
 };
 using xbasic_system_contract_t = xtop_basic_system_contract;
 
