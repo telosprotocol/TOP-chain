4981971da4b535cf66282e79b171a76976e81772
LonDong <lon.dong@topnetwork.org>
2021-09-03 09:59:12 +0800

    add contract deploy

--- src/xtopcom/xsystem_contracts/xsystem_contract_addresses.h
+++ src/xtopcom/xsystem_contracts/xsystem_contract_addresses.h
@@ -6,4 +6,8 @@ NS_BEG2(top, system_contracts)
 
 XINLINE_CONSTEXPR char const * transfer_address{"T20000MAZNFs3aSZGDdVNvRvpbvKwhWDWvuXa484"};
 
+XINLINE_CONSTEXPR char const * sys_contract_beacon_timer_addr{"Tt00013axZ3Gy8nzi7oNYhTBDb9XMb8KHdqYhw4Kx"};
+XINLINE_CONSTEXPR char const * sys_contract_rec_registration_addr{ "T2000138NZjvNJjRNG5iEqVKydpqAqoeNjBuFmNbj@0" };
+XINLINE_CONSTEXPR char const * sys_contract_rec_standby_pool_addr{ "T2000138CQwyzFxbWZ59mNjkq3eZ3eH41t7b5midm@0" };
+
 NS_END2
