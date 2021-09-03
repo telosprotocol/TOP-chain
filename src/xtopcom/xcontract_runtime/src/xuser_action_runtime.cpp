71b7cda92a5aa5bd4cd7fdc3acfb57f8a4c3b0cd
LonDong <lon.dong@topnetwork.org>
2021-09-03 09:56:43 +0800

    make compiled

--- src/xtopcom/xcontract_runtime/src/xuser_action_runtime.cpp
+++ src/xtopcom/xcontract_runtime/src/xuser_action_runtime.cpp
@@ -22,18 +22,18 @@ xtransaction_execution_result_t xtop_action_runtime<xuser_consensus_action_t>::e
     xtransaction_execution_result_t result;
 
     try {
-        if (exe_ctx->transaction_type() == data::enum_xtransaction_type::xtransaction_type_deploy_wasm_contract) {
-            auto action_data = exe_ctx->action_data();
-            uint64_t tgas_limit{ 0 };
-            xbyte_buffer_t code;
-            base::xstream_t stream(base::xcontext_t::instance(), action_data.data(), action_data.size());
-            stream >> tgas_limit;
-            stream >> code;
-            auto engine = std::make_shared<user::xwasm_engine_t>();
-            engine->deploy_contract(code, exe_ctx);
-        } else {
+        // if (exe_ctx->transaction_type() == data::enum_xtransaction_type::xtransaction_type_deploy_wasm_contract) {
+        //     auto action_data = exe_ctx->action_data();
+        //     uint64_t tgas_limit{ 0 };
+        //     xbyte_buffer_t code;
+        //     base::xstream_t stream(base::xcontext_t::instance(), action_data.data(), action_data.size());
+        //     stream >> tgas_limit;
+        //     stream >> code;
+        //     auto engine = std::make_shared<user::xwasm_engine_t>();
+        //     engine->deploy_contract(code, exe_ctx);
+        // } else {
 
-        }
+        // }
     } catch (top::error::xtop_error_t const & eh) {
         result.status.ec = eh.code();
     } catch (std::exception const & eh) {
