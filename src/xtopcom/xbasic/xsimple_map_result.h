b103002deb68baf710f14b4a2af5c127f202095a
Charles.Liu <charles.liu@topnetwork.org>
2021-09-22 18:19:37 +0800

    add xsimple_map_test

--- src/xtopcom/xbasic/xsimple_map_result.h
+++ src/xtopcom/xbasic/xsimple_map_result.h
@@ -8,7 +8,23 @@
 
 #include <map>
 
+// #include <type_traits>
+
 NS_BEG1(top)
+
+// template<class T, class EqualTo>
+// struct has_operator_equal_impl
+// {
+//     template<class U, class V>
+//     static auto test(U*) -> decltype(std::declval<U>() == std::declval<V>());
+//     template<typename, typename>
+//     static auto test(...) -> std::false_type;
+
+//     using type = typename std::is_same<bool, decltype(test<T, EqualTo>(0))>::type;
+// };
+
+// template<class T, class EqualTo = T>
+// struct has_operator_equal : has_operator_equal_impl<T, EqualTo>::type {};
 
 template <typename KeyT, typename ValueT>
 class xtop_simple_map_result final {
@@ -30,12 +46,30 @@ public:
     typedef typename container_t::iterator iterator;
     typedef typename container_t::const_iterator const_iterator;
 
+    // insert would fail when key_type already exist.
+    // if you want to update some value_type, use `update`
     std::pair<iterator, bool> insert(value_type const & value) {
         return m_result.insert(value);
     }
 
     std::pair<iterator, bool> insert(value_type && value) {
         return m_result.insert(std::move(value));
+    }
+
+    std::pair<iterator, bool> update(value_type const & value) {
+        if (m_result.find(value.first) == m_result.end() || m_result[value.first] == value.second) {
+            return m_result.insert(value);
+        }
+        m_result.at(value.first) = value.second;
+        return std::make_pair(m_result.find(value.first), true);
+    }
+
+    std::pair<iterator, bool> update(value_type && value) {
+        if (m_result.find(value.first) == m_result.end() || m_result[value.first] == value.second) {
+            return m_result.insert(std::move(value));
+        }
+        m_result.at(value.first) = value.second;
+        return std::make_pair(m_result.find(value.first), true);
     }
 
     bool empty(key_type const & key) const noexcept {
