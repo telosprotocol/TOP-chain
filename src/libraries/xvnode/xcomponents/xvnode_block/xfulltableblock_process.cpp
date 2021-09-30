a22f555ec7913c74457fcb4f177de1a94d3166a9
wens.weng <wens.wq@gmail.com>
2021-09-30 14:17:49 +0800

    feat(contract): update fulltableblock

--- src/libraries/xvnode/xcomponents/xvnode_block/xfulltableblock_process.cpp
+++ src/libraries/xvnode/xcomponents/xvnode_block/xfulltableblock_process.cpp
@@ -1,18 +1,30 @@
-#include "xvnode/xcomponents/xvnode_fulltableblock/xfulltableblock_process.h"
-
-#include <iostream>
+#include "xvnode/xcomponents/xvnode_block/xfulltableblock_process.h"
 
 NS_BEG3(top, vnode, components)
-using namespace top::data;
 
+xvblock_class_t xfulltableblock_process_t::block_class(xobject_ptr_t<base::xvblock_t> const & vblock) {
+    return vblock->get_block_class();
+}
 
-xfulltableblock_statistic_accounts fulltableblock_statistic_accounts(xstatistics_data_t const& block_statistic_data, base::xvnodesrv_t * node_service) {
+xvblock_level_t xfulltableblock_process_t::block_level(xobject_ptr_t<base::xvblock_t> const & vblock) {
+    return vblock->get_block_level();
+}
+
+void  xfulltableblock_process_t::process_block(xobject_ptr_t<base::xvblock_t> const & vblock) {
+    assert(vblock->get_block_class() == xvblock_class_t::enum_xvblock_class_full &&
+            vblock->get_block_level() == xvblock_level_t::enum_xvblock_level_table);
+
+    return;
+}
+
+data::xfulltableblock_statistic_accounts xfulltableblock_process_t::fulltableblock_statistic_accounts(data::xstatistics_data_t const& block_statistic_data, base::xvnodesrv_t * node_service) {
+    using namespace top::data;
+
     xfulltableblock_statistic_accounts res;
 
     // process one full tableblock statistic data
     for (auto const & statistic_item: block_statistic_data.detail) {
         auto elect_statistic = statistic_item.second;
-        // std::cout << "fulltableblock_statistic_accounts: group size" << elect_statistic.group_statistics_data.size() << "\n";
         xfulltableblock_group_data_t res_group_data;
         for (auto const & group_item: elect_statistic.group_statistics_data) {
             xgroup_related_statistics_data_t const& group_account_data = group_item.second;
@@ -26,7 +38,6 @@ xfulltableblock_statistic_accounts fulltableblock_statistic_accounts(xstatistics
                 statistic_item.first
             };
 
-            // std::cout << "fulltableblock_statistic_accounts: xvip2" << group_addr.to_string() << "\n";
             xfulltableblock_account_data_t res_account_data;
             for (std::size_t slotid = 0; slotid < group_account_data.account_statistics_data.size(); ++slotid) {
                 auto account_addr = node_service->get_group(group_xvip2)->get_node(slotid)->get_account();
