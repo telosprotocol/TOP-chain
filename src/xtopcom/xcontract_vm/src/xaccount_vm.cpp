236c11c7e374dfba51d49080df7c916c883b9637
LonDong <lon.dong@topnetwork.org>
2021-10-11 16:01:39 +0800

    fix(contract): fix vm nonce mismatch

--- src/xtopcom/xcontract_vm/src/xaccount_vm.cpp
+++ src/xtopcom/xcontract_vm/src/xaccount_vm.cpp
@@ -32,13 +32,13 @@ std::vector<xaccount_vm_output_t> xtop_account_vm::execute(std::vector<xaccount_
 }
 
 xaccount_vm_output_t xtop_account_vm::execute(std::vector<data::xcons_transaction_ptr_t> const & txs,
-                                                        xobject_ptr_t<base::xvbstate_t> block_state,
-                                                        data::xblock_consensus_para_t const & cs_para) {
+                                              xobject_ptr_t<base::xvbstate_t> block_state,
+                                              data::xblock_consensus_para_t const & cs_para) {
     xaccount_vm_execution_result_t result;
     const size_t result_size = txs.size();
     assert(result_size == 1);
     result.transaction_results.reserve(result_size);
-    
+
     const std::vector<data::xcons_transaction_ptr_t> txs_for_actions(txs);
 
     state_accessor::xstate_access_control_data_t ac_data;  // final get from config or program initialization start
@@ -80,30 +80,26 @@ contract_runtime::xtransaction_execution_result_t xtop_account_vm::execute_actio
     contract_runtime::xtransaction_execution_result_t result;
 
     switch (action->type()) {
-    case data::xtop_action_type_t::system:
-    {
+    case data::xtop_action_type_t::system: {
         auto const * cons_action = static_cast<data::xsystem_consensus_action_t const *>(action.get());
-        contract_common::xcontract_state_t contract_state{ cons_action->contract_address(), make_observer(std::addressof(ac)) };
+        contract_common::xcontract_state_t contract_state{cons_action->contract_address(), make_observer(std::addressof(ac))};
         result = sys_action_runtime_->new_session(make_observer(std::addressof(contract_state)))->execute_action(std::move(action));
         break;
     }
 
-    case data::xtop_action_type_t::user:
-    {
+    case data::xtop_action_type_t::user: {
         // auto const & action = dynamic_cast<data::xuser_consensus_action_t const &>(actions[i]);
         // contract_common::xcontract_state_t contract_state{ action.contract_address(), make_observer(std::addressof(ac)) };
         // result.transaction_results[i] = user_action_runtime_->new_session(make_observer(std::addressof(contract_state)))->execute_action(action, param);
         break;
     }
 
-    case data::xtop_action_type_t::event:
-    {
+    case data::xtop_action_type_t::event: {
         // TODO: we don't have event action either. this is just a placeholder.
         break;
     }
 
-    default:
-    {
+    default: {
         xerror("[xtop_account_vm::execute_action] error action type: %d", action->type());
         assert(false);  // NOLINT(clang-diagnostic-disabled-macro-expansion)
         result.status.ec = error::xerrc_t::invalid_contract_type;
@@ -124,11 +120,14 @@ contract_runtime::xtransaction_execution_result_t xtop_account_vm::execute_actio
                     break;
                 }
                 follow_up.execute_type = contract_common::xfollowup_transaction_execute_type_t::success;
+                result.output.binlog = follow_up_result.output.binlog;
+                result.output.contract_state_snapshot = follow_up_result.output.contract_state_snapshot;
                 for (auto & double_follow_up : follow_up_result.output.followup_transaction_data) {
                     result.output.followup_transaction_data.emplace_back(std::move(double_follow_up));
+                    // not support double follow up now
+                    assert(false);
                 }
             } else if (follow_up.execute_type == contract_common::xfollowup_transaction_execute_type_t::success) {
-
             } else {
                 xerror("[xtop_account_vm::execute_action] error follow up tx execute type: %d", follow_up.execute_type);
                 assert(false);
@@ -154,8 +153,8 @@ void xtop_account_vm::abort(const size_t start_index, const size_t size, xaccoun
 }
 
 xaccount_vm_output_t xtop_account_vm::pack(std::vector<data::xcons_transaction_ptr_t> const & txs,
-                                             xaccount_vm_execution_result_t const & result,
-                                             contract_common::properties::xproperty_access_control_t & ac) {
+                                           xaccount_vm_execution_result_t const & result,
+                                           contract_common::properties::xproperty_access_control_t & ac) {
     xaccount_vm_output_t output;
     std::vector<data::xcons_transaction_ptr_t> output_txs(txs);
     uint32_t send_tx_num{0};
@@ -165,7 +164,7 @@ xaccount_vm_output_t xtop_account_vm::pack(std::vector<data::xcons_transaction_p
     auto last_nonce = ac.latest_sendtx_nonce();
     auto last_hash = ac.latest_sendtx_hash();
 
-    uint32_t last_success_tx_index{0};
+    // uint32_t last_success_tx_index{0};
     for (size_t i = 0; i < result.transaction_results.size(); i++) {
         auto const & r = result.transaction_results[i];
         auto & tx = output_txs[i];
@@ -175,7 +174,7 @@ xaccount_vm_output_t xtop_account_vm::pack(std::vector<data::xcons_transaction_p
                 output.failed_tx_assemble.emplace_back(tx);
             } else if (tx->is_recv_tx()) {
                 output.success_tx_assemble.emplace_back(tx);
-                last_success_tx_index = i;
+                // last_success_tx_index = i;
             } else {
                 xwarn("[xlightunit_builder_t::build_block] invalid tx type: %d", tx->get_tx_type());
                 assert(false);
@@ -183,7 +182,7 @@ xaccount_vm_output_t xtop_account_vm::pack(std::vector<data::xcons_transaction_p
         } else {
             tx->set_current_exec_status(data::enum_xunit_tx_exec_status::enum_xunit_tx_exec_status_success);
             output.success_tx_assemble.emplace_back(tx);
-            last_success_tx_index = i;
+            // last_success_tx_index = i;
             for (auto & follow_up : r.output.followup_transaction_data) {
                 auto const & follow_up_tx = follow_up.followed_transaction;
                 if (follow_up.schedule_type == contract_common::xfollowup_transaction_schedule_type_t::immediately) {
@@ -229,11 +228,11 @@ xaccount_vm_output_t xtop_account_vm::pack(std::vector<data::xcons_transaction_p
 
     // set unconfirm sendtx number
     uint32_t old_unconfirm_tx_num = ac.unconfirm_sendtx_num();
-    // printf("old_unconfirm_num=%d,send_tx_num=%d,recv_tx_num=%d,confirm_tx_num=%d\n",
-    //         old_unconfirm_tx_num, send_tx_num, recv_tx_num, confirm_tx_num);
     if (old_unconfirm_tx_num + send_tx_num < confirm_tx_num) {
         xerror("xaccount_context_t::finish_exec_all_txs fail-unconfirm num unmatch.old_unconfirm_num=%d,send_tx_num=%d,confirm_tx_num=%d",
-            old_unconfirm_tx_num, send_tx_num, confirm_tx_num);
+               old_unconfirm_tx_num,
+               send_tx_num,
+               confirm_tx_num);
         output.status.ec = contract_vm::error::xenum_errc::confirm_tx_num_error;
         return output;
     }
@@ -244,8 +243,10 @@ xaccount_vm_output_t xtop_account_vm::pack(std::vector<data::xcons_transaction_p
     }
 
     output.unconfirm_sendtx_num = new_unconfirm_new;
-    output.binlog = result.transaction_results[last_success_tx_index].output.binlog;
-    output.contract_state_snapshot = result.transaction_results[last_success_tx_index].output.contract_state_snapshot;
+    // output.binlog = result.transaction_results[last_success_tx_index].output.binlog;
+    // output.contract_state_snapshot = result.transaction_results[last_success_tx_index].output.contract_state_snapshot;
+    output.binlog = ac.binlog();
+    output.contract_state_snapshot = ac.fullstate_bin();
     assert(!output.binlog.empty());
     assert(!output.contract_state_snapshot.empty());
 
