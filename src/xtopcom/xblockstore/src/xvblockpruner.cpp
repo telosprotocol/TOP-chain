ad996e8a1611e19e714fc10b6fbf5ffa8df3f39d
forme628 <forme.he@topnetwork.org>
2021-11-16 16:05:17 +0800

    feat(blockstore):table_prune

--- src/xtopcom/xblockstore/src/xvblockpruner.cpp
+++ src/xtopcom/xblockstore/src/xvblockpruner.cpp
@@ -70,29 +70,39 @@ namespace top
         {
             if(account_meta._highest_full_block_height <= enum_reserved_blocks_count) //start prune at least > 8
                 return false;
-    
-            return false;//XTODO,disable table_prune temporary
             
             //[lower_bound_height,upper_bound_height)
-            const uint64_t upper_bound_height = account_meta._highest_full_block_height - enum_reserved_blocks_count;
+            uint64_t upper_bound_height = account_meta._highest_full_block_height - enum_reserved_blocks_count;
             const uint64_t lower_bound_height = std::max(account_meta._lowest_vkey2_block_height,account_meta._highest_deleted_block_height) + 1;
             
-            xinfo("xvblockprune_impl::recycle account %s, upper %llu, lower %llu, connect_height %llu", account_obj.get_address().c_str(),
+            xdbg("xvblockprune_impl::recycle account %s, upper %llu, lower %llu, connect_height %llu", account_obj.get_address().c_str(),
                 upper_bound_height, lower_bound_height, account_meta._highest_connect_block_height);
             
             if(lower_bound_height >= upper_bound_height)
                 return false;
-            else if((upper_bound_height - lower_bound_height) < 5000)
+            else if((upper_bound_height - lower_bound_height) < (enum_min_batch_recycle_blocks_count << 1))
                 return false;//collect big range for each prune op as performance consideration
             
+            upper_bound_height = upper_bound_height - (enum_min_batch_recycle_blocks_count << 1);
+
+            uint64_t boundary = get_prune_boundary(account_obj);
+            if (boundary < upper_bound_height) {
+                upper_bound_height = boundary;
+            }
+
+            xdbg("xvblockprune_impl::recycle account %s, adjust upper %llu, lower %llu, connect_height %llu", account_obj.get_address().c_str(),
+                upper_bound_height, lower_bound_height, account_meta._highest_connect_block_height);
+            if (lower_bound_height >= upper_bound_height) {
+                return false;
+            }
+
             const std::string begin_delete_key = base::xvdbkey_t::create_prunable_block_height_key(account_obj,lower_bound_height);
-            const std::string end_delete_key = base::xvdbkey_t::create_prunable_block_height_key(account_obj,upper_bound_height - 5000);
+            const std::string end_delete_key = base::xvdbkey_t::create_prunable_block_height_key(account_obj,upper_bound_height);
             if(get_xvdb()->delete_range(begin_delete_key, end_delete_key))//["begin_key", "end_key")
             {
                 xinfo("xvblockprune_impl::recycle,succsssful for account %s between %s and %s", account_obj.get_address().c_str(), begin_delete_key.c_str(), end_delete_key.c_str());
                 
-                account_meta._highest_deleted_block_height = upper_bound_height - 5000 + 1;
-                return true;//return true to indicate it has successful deleted
+                account_meta._highest_deleted_block_height = upper_bound_height - 1;
             }
             else
             {
@@ -105,11 +115,13 @@ namespace top
         {
             if(account_meta._highest_full_block_height <= enum_reserved_blocks_count) //start prune at least > 8
                 return false;
- 
+             
             //[lower_bound_height,upper_bound_height)
-            const uint64_t upper_bound_height = account_meta._highest_full_block_height - enum_reserved_blocks_count;
+
+            uint64_t upper_bound_height = account_meta._highest_full_block_height - enum_reserved_blocks_count;
             const uint64_t lower_bound_height = std::max(account_meta._lowest_vkey2_block_height,account_meta._highest_deleted_block_height) + 1;
             
+
             xdbg("xvblockprune_impl::recycle account %s, upper %llu, lower %llu, connect_height %llu", account_obj.get_address().c_str(),
                 upper_bound_height, lower_bound_height, account_meta._highest_connect_block_height);
             
@@ -118,20 +130,122 @@ namespace top
             else if((upper_bound_height - lower_bound_height) < enum_min_batch_recycle_blocks_count)
                 return false;//collect big range for each prune op as performance consideration
             
+            upper_bound_height = upper_bound_height - enum_min_batch_recycle_blocks_count;
+
             const std::string begin_delete_key = base::xvdbkey_t::create_prunable_block_height_key(account_obj,lower_bound_height);
             const std::string end_delete_key = base::xvdbkey_t::create_prunable_block_height_key(account_obj,upper_bound_height);
             if(get_xvdb()->delete_range(begin_delete_key, end_delete_key))//["begin_key", "end_key")
             {
+
                 xinfo("xvblockprune_impl::recycle,succsssful for account %s between %s and %s", account_obj.get_address().c_str(), begin_delete_key.c_str(), end_delete_key.c_str());
                 
                 account_meta._highest_deleted_block_height = upper_bound_height - 1;
-                return true;//return true to indicate it has successful deleted
             }
             else
             {
                 xerror("xvblockprune_impl::recycle,failed for account %s between %s and %s", account_obj.get_address().c_str(), begin_delete_key.c_str(), end_delete_key.c_str());
             }
+
             return false;
+        }
+
+        bool  xvblockprune_impl::refresh(const chainbase::enum_xmodule_type mod_id, const base::xvaccount_t & account_obj, const uint64_t permit_prune_upper_boundary) {
+            if(!account_obj.is_table_address()) {
+                return false;
+            }
+
+            // if consensus zone
+            auto zone_id = account_obj.get_zone_index();
+            if ((zone_id == base::enum_chain_zone_zec_index) || (zone_id == base::enum_chain_zone_beacon_index)) {
+                return false;
+            }
+            
+            xdbg("xvblockprune_impl::refresh, account:%s, module:%llx, height:%llu", account_obj.get_address().c_str(), mod_id, permit_prune_upper_boundary);
+            
+            //设置lock为1
+            std::unique_lock<std::mutex> lock(m_lock);
+            auto mod_prune_boundary = m_prune_boundary.find(account_obj.get_account());
+            if (mod_prune_boundary != m_prune_boundary.end()) {
+                if (mod_prune_boundary->second.find(mod_id) != mod_prune_boundary->second.end()) {
+                    if (mod_prune_boundary->second[mod_id] >= permit_prune_upper_boundary) {
+                        return true;
+                    }
+                } 
+                mod_prune_boundary->second[mod_id] = permit_prune_upper_boundary;
+            } else {
+                std::map<chainbase::enum_xmodule_type, uint64_t> prune_boundary;
+                prune_boundary[mod_id] = permit_prune_upper_boundary;
+                m_prune_boundary[account_obj.get_account()] = prune_boundary;
+            }
+            
+            return true;
+        }
+
+        bool  xvblockprune_impl::watch(const chainbase::enum_xmodule_type mod_id, const base::xvaccount_t & account_obj) {
+            if(!account_obj.is_table_address()) {
+                return false;
+            }
+
+            // if consensus zone
+            auto zone_id = account_obj.get_zone_index();
+            if ((zone_id == base::enum_chain_zone_zec_index) || (zone_id == base::enum_chain_zone_beacon_index)) {
+                return false;
+            }
+
+            xdbg("xvblockprune_impl::watch, account:%s, module:%llx", account_obj.get_address().c_str(), mod_id);
+
+            //设置lock为1
+            std::unique_lock<std::mutex> lock(m_lock);  
+            auto mod_prune_boundary = m_prune_boundary.find(account_obj.get_account());
+            if (mod_prune_boundary == m_prune_boundary.end()) {
+                std::map<chainbase::enum_xmodule_type, uint64_t> prune_boundary;
+                prune_boundary[mod_id] = 0;
+                m_prune_boundary[account_obj.get_account()] = prune_boundary;
+            }
+            return true;
+        }
+
+        bool  xvblockprune_impl::unwatch(const chainbase::enum_xmodule_type mod_id, const base::xvaccount_t & account_obj) {
+            if(!account_obj.is_table_address()) {
+                return false;
+            }
+
+            // if consensus zone
+            auto zone_id = account_obj.get_zone_index();
+            if ((zone_id == base::enum_chain_zone_zec_index) || (zone_id == base::enum_chain_zone_beacon_index)) {
+                return false;
+            }
+
+            xdbg("xvblockprune_impl::unwatch, account:%s, module:%llx", account_obj.get_address().c_str(), mod_id);
+
+            //设置lock为1
+            std::unique_lock<std::mutex> lock(m_lock);  
+            auto mod_prune_boundary = m_prune_boundary.find(account_obj.get_account());
+            if (mod_prune_boundary != m_prune_boundary.end()) {
+                mod_prune_boundary->second.erase(mod_id);
+                if (mod_prune_boundary->second.empty()) {
+                    m_prune_boundary.erase(account_obj.get_account());
+                }
+            }
+
+            return true;
+        }
+
+        uint64_t xvblockprune_impl::get_prune_boundary(const base::xvaccount_t & account_obj) {
+            //设置lock为1
+            std::unique_lock<std::mutex> lock(m_lock); 
+            auto mod_prune_boundary = m_prune_boundary.find(account_obj.get_account());
+            if (mod_prune_boundary == m_prune_boundary.end()) {
+                return 0;
+            }
+            
+            uint64_t min_prune_boundary = (uint64_t)-1;
+            for (auto it : mod_prune_boundary->second) {
+                if (it.second < min_prune_boundary) {
+                    min_prune_boundary = it.second;
+                }
+            }
+            return min_prune_boundary;
         }
     }
 }
