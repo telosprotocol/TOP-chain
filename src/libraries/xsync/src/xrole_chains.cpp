0a818b298ae0a501a49943e45f363ee66bd1cf7b
Hench <hench.ye@topnetwork.org>
2021-11-23 15:02:19 +0800

    fix roles

--- src/libraries/xsync/src/xrole_chains.cpp
+++ src/libraries/xsync/src/xrole_chains.cpp
@@ -46,7 +46,7 @@ void xrole_chains_t::init_chains() {
     add_tables(nt::storage_archive, sys_contract_beacon_table_block_addr, enum_chain_sync_policy_full);
     add_tables(nt::storage_archive, sys_contract_zec_table_block_addr, enum_chain_sync_policy_full);
     add_tables(nt::storage_archive, sys_contract_sharding_table_block_addr, enum_chain_sync_policy_full);
-    add_tables(nt::storage_archive, sys_drand_addr, enum_chain_sync_policy_full);
+    add_chain(nt::storage_archive, sys_drand_addr, enum_chain_sync_policy_full);
 
     add_tables(nt::storage_full_node, sys_contract_beacon_table_block_addr, enum_chain_sync_policy_full);
     add_tables(nt::storage_full_node, sys_contract_zec_table_block_addr, enum_chain_sync_policy_full);
