287568d80cccce100b7178041224fb0a8da35749
Hench <hench.ye@topnetwork.org>
2021-11-24 19:17:35 +0800

    fix xchain_downloader_t

--- src/libraries/xsync/src/xchain_downloader.cpp
+++ src/libraries/xsync/src/xchain_downloader.cpp
@@ -477,7 +477,9 @@ xsync_command_execute_result xchain_downloader_t::execute_download(uint64_t star
                 xsync_info("chain_downloader on_behind(depend chain is syncing) %s,height=%lu,", m_address.c_str(), end_height);
                 return abort;
             }
+        } else if (m_address == sys_drand_addr) {
         } else {
+            xsync_dbg("xchain_downloader_t::execute_download check fail: %s", m_address.c_str());
             return abort;
         }
     }
