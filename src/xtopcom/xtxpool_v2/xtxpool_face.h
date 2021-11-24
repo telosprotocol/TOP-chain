c3aac13415fa481c1ab2e426f7659b9cfb502613
nathan <nathan.sun@topnetwork.org>
2021-11-24 15:07:15 +0800

    feat(txpool/us):cache unconfirmed raw tx in txpool

--- src/xtopcom/xtxpool_v2/xtxpool_face.h
+++ src/xtopcom/xtxpool_v2/xtxpool_face.h
@@ -258,6 +258,7 @@ public:
     virtual void print_statistic_values() const = 0;
     virtual void update_peer_receipt_id_state(const base::xreceiptid_state_ptr_t & receiptid_state) = 0;
     virtual std::map<std::string, uint64_t> get_min_keep_heights() const = 0;
+    virtual xtransaction_ptr_t get_raw_tx(const std::string & account_addr, base::xtable_shortid_t peer_table_sid, uint64_t receipt_id) const = 0;
 };
 
 class xtxpool_instance {
