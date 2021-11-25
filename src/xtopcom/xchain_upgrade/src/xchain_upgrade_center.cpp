df482b9061bf53477c601309060d35ecef81ab29
PaytonWu <payton.wu@outlook.com>
2021-11-25 15:25:42 +0800

    feat(chain_fork): fork datetime for v1.2.6

--- src/xtopcom/xchain_upgrade/src/xchain_upgrade_center.cpp
+++ src/xtopcom/xchain_upgrade/src/xchain_upgrade_center.cpp
@@ -35,23 +35,23 @@ namespace top {
         };
 #else   // #if defined(XCHAIN_FORKED_BY_DEFAULT)
         xchain_fork_config_t  mainnet_chain_config{
-            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "block fork point for table receipt"},
-            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table statistic info fork point"},
-            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table receipt protocol fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 6513480, "block fork point for table receipt"},
+            xfork_point_t{xfork_point_type_t::logic_time, 6522120, "table statistic info fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 6522480, "table receipt protocol fork point"},
         };
 
         // !!!change!!! fork time for galileo
         xchain_fork_config_t  testnet_chain_config{
-            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "block fork point for table receipt"},
-            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table statistic info fork point"},
-            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table receipt protocol fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 6513480, "block fork point for table receipt"},
+            xfork_point_t{xfork_point_type_t::logic_time, 6522120, "table statistic info fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 6522480, "table receipt protocol fork point"},
         };
 
         // !!!change!!! fork time for local develop net
         xchain_fork_config_t default_chain_config {
-            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "block fork point for table receipt"},
-            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table statistic info fork point"},
-            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table receipt protocol fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 6513480, "block fork point for table receipt"},
+            xfork_point_t{xfork_point_type_t::logic_time, 6522120, "table statistic info fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 6522480, "table receipt protocol fork point"},
         };
 #endif  // #if defined(XCHAIN_FORKED_BY_DEFAULT)
 
