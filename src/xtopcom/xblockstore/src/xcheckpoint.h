ac74fc14072c22363fadd8c5980f1f0d17814df5
LonDong <lon.dong@topnetwork.org>
2021-12-07 18:37:04 +0800

    feat(checkpoint): latest checkpoint verify && test

--- src/xtopcom/xblockstore/src/xcheckpoint.h
+++ src/xtopcom/xblockstore/src/xcheckpoint.h
@@ -10,13 +10,7 @@
 #include <string>
 
 namespace top {
-namespace chain_checkpoint {
-
-struct xtop_checkpoint_info {
-    uint64_t height;
-    std::string hash;
-};
-using xcheckpoint_info_t = xtop_checkpoint_info;
+namespace store {
 
 struct xtop_checkpoint_cmp {
     bool operator()(const uint64_t & lhs, const uint64_t & rhs) const {
@@ -25,17 +19,17 @@ struct xtop_checkpoint_cmp {
 };
 using xcheckpoint_cmp_t = xtop_checkpoint_cmp;
 
-using xcheckpoints_t = std::map<uint64_t, xcheckpoint_info_t, xcheckpoint_cmp_t>;
+using xcheckpoints_t = std::map<uint64_t, std::string, xcheckpoint_cmp_t>;
 using xcheckpoints_map_t = std::map<std::string, xcheckpoints_t>;
 
 class xtop_chain_checkpoint {
 public:
-    static xcheckpoints_t const checkpoints(std::string account);
+    static xcheckpoints_t const & checkpoints(std::string const & account);
 
 private:
     static xcheckpoints_map_t m_checkpoints_map;
 };
 using xchain_checkpoint_t = xtop_chain_checkpoint;
 
-}  // namespace chain_checkpoint
+}  // namespace store
 }  // namespace top
---
+++ src/xtopcom/xblockstore/xcheckpoint_data/xcheckpoint_new_horizons.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace top {
+namespace store {
+static auto checkpoint_json =
+    R"T(
+{
+}
+)T";
+}
+}
