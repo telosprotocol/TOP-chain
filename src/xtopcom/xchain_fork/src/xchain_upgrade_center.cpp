33035e016de7861b2ec93d5acb699258c6fdb913
wens.weng <wens.wq@gmail.com>
2021-12-10 16:28:42 +0800

    feat(contract): initial creditscrore

--- src/xtopcom/xchain_fork/src/xchain_upgrade_center.cpp
+++ src/xtopcom/xchain_fork/src/xchain_upgrade_center.cpp
@@ -20,6 +20,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 0, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "tx v2 fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "blacklist function fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 0, "node_initial_credit_fork_point"},
         };
 
         // !!!change!!! fork time for galileo
@@ -28,6 +29,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 0, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "tx v2 fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "blacklist function fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 0, "node_initial_credit_fork_point"},
         };
 
         // !!!change!!! fork time for local develop net
@@ -36,6 +38,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 0, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "tx v2 fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "blacklist function fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 0, "node_initial_credit_fork_point"},
         };
 #else   // #if defined(XCHAIN_FORKED_BY_DEFAULT)
         xchain_fork_config_t  mainnet_chain_config{
@@ -43,6 +46,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "tx v2 fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 15000000, "blacklist function fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 16000000, "node_initial_credit_fork_point"},
         };
 
         // !!!change!!! fork time for galileo
@@ -51,6 +55,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "tx v2 fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 15000000, "blacklist function fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 16000000, "node_initial_credit_fork_point"},
         };
 
         // !!!change!!! fork time for local develop net
@@ -59,6 +64,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "tx v2 fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 15000000, "blacklist function fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 16000000, "node_initial_credit_fork_point"},
         };
 #endif  // #if defined(XCHAIN_FORKED_BY_DEFAULT)
 
