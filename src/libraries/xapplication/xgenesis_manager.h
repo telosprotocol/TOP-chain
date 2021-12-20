19ec0403ef2c986f228245d295216b4d60098980
LonDong <lon.dong@topnetwork.org>
2021-12-20 11:38:16 +0800

    fix(genesis): fix dead lock

--- src/libraries/xapplication/xgenesis_manager.h
+++ src/libraries/xapplication/xgenesis_manager.h
@@ -10,6 +10,12 @@
 
 NS_BEG2(top, application)
 
+enum xtop_enum_create_src {
+    init,
+    blockstore,
+};
+using xenum_create_src_t = xtop_enum_create_src;
+
 class xtop_genesis_manager {
 public:
     xtop_genesis_manager(observer_ptr<base::xvblockstore_t> const & blockstore, observer_ptr<store::xstore_face_t> const & store);
@@ -32,29 +38,29 @@ private:
     /// @brief Create genesis block of root account.
     /// @param account Root account.
     /// @param ec Log the error code.
-    void create_genesis_of_root_account(base::xvaccount_t const & account, std::error_code & ec);
+    void create_genesis_of_root_account(base::xvaccount_t const & account, xenum_create_src_t src, std::error_code & ec);
 
     /// @brief Create genesis block of contract account.
     /// @param account Contract account.
     /// @param ec Log the error code.
-    void create_genesis_of_contract_account(base::xvaccount_t const & account, std::error_code & ec);
+    void create_genesis_of_contract_account(base::xvaccount_t const & account, xenum_create_src_t src, std::error_code & ec);
 
     /// @brief Create genesis block of genesis account.
     /// @param account Genesis account.
     /// @param data Data of genesis account, which means balance.
     /// @param ec Log the error code.
-    void create_genesis_of_genesis_account(base::xvaccount_t const & account, uint64_t const data, std::error_code & ec);
+    void create_genesis_of_genesis_account(base::xvaccount_t const & account, uint64_t const data, xenum_create_src_t src, std::error_code & ec);
 
     /// @brief Create genesis block of user account who has specific data.
     /// @param account User account.
     /// @param data Data of user account which recorded in file "xchain_data_new_horizons.h".
     /// @param ec Log the error code.
-    void create_genesis_of_datauser_account(base::xvaccount_t const & account, chain_data::data_processor_t const & data, std::error_code & ec);
+    void create_genesis_of_datauser_account(base::xvaccount_t const & account, chain_data::data_processor_t const & data, xenum_create_src_t src, std::error_code & ec);
 
     /// @brief Create genesis block of none data account.
     /// @param account Common account.
     /// @param ec Log the error code.
-    void create_genesis_of_common_account(base::xvaccount_t const & account, std::error_code & ec);
+    void create_genesis_of_common_account(base::xvaccount_t const & account, xenum_create_src_t src, std::error_code & ec);
 
     /// @brief Load accounts of different types.
     void load_accounts();
@@ -72,6 +78,7 @@ private:
     std::set<common::xaccount_address_t> m_contract_accounts;
     std::map<common::xaccount_address_t, uint64_t> m_genesis_accounts_data;
     std::map<common::xaccount_address_t, chain_data::data_processor_t> m_user_accounts_data;
+    std::set<common::xaccount_address_t> m_exist_accounts;
 };
 using xgenesis_manager_t = xtop_genesis_manager;
 
