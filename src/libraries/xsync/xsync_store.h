5174543d147a164c39f44dd39a7c72dacc821d01
forme628 <forme.he@topnetwork.org>
2021-12-31 14:54:45 +0800

    feat(fullnode):update

--- src/libraries/xsync/xsync_store.h
+++ src/libraries/xsync/xsync_store.h
@@ -55,6 +55,7 @@ public:
     virtual bool set_unit_proof(const base::xvaccount_t & account, const std::string & unit_proof, uint64_t height) = 0;
     virtual const std::string get_unit_proof(const base::xvaccount_t & account, uint64_t height) = 0;
     virtual bool remove_empty_unit_forked() = 0;
+    virtual bool is_full_node_forked() = 0;
     const static uint64_t m_undeterministic_heights = 2;
 };
 
@@ -125,12 +126,14 @@ public:
     bool set_unit_proof(const base::xvaccount_t & account, const std::string & unit_proof, uint64_t height) override;
     const std::string get_unit_proof(const base::xvaccount_t & account, uint64_t height) override;
     bool remove_empty_unit_forked() override;
-    
+    bool is_full_node_forked() override;
 private:
+    void set_fork_point();
     std::string m_vnode_id;
     observer_ptr<base::xvblockstore_t> m_blockstore{};
     xsync_store_shadow_t *m_shadow;
     bool m_remove_empty_unit_forked{false};
+    bool m_full_node_forked{false};
 };
 
 NS_END2
