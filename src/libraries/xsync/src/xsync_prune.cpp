cb7b6eadb56e9d2d6b59b1fca645e1a9181364ab
forme628 <forme.he@topnetwork.org>
2022-01-04 20:03:40 +0800

    feat(fullnode):fullnode doesnot prune

--- src/libraries/xsync/src/xsync_prune.cpp
+++ src/libraries/xsync/src/xsync_prune.cpp
@@ -6,53 +6,82 @@
 
 NS_BEG2(top, sync)
 
-void xsync_prune_t::add(const std::string address) {
+void xsync_prune_t::add(const std::string address, const std::set<enum_height_type> types) {
     std::unique_lock<std::mutex> lock(m_lock);
-    auto prune_account = m_min_height_accounts.find(address);
-    if (prune_account != m_min_height_accounts.end()) {
+    auto prune_account = m_prune_accounts.find(address);
+    if (prune_account != m_prune_accounts.end()) {
+        for (auto type:types) {
+            if (prune_account->second.find((enum_height_type)type) == prune_account->second.end()) {
+                prune_account->second[(enum_height_type)type] = 0;
+                m_min_height_accounts[address] = 0;
+            }
+        }
+    } else {
+        m_min_height_accounts[address] = 0;
+        std::map<enum_height_type, uint64_t> prune;
+        for (auto type:types) {
+            prune[(enum_height_type)type] = 0;
+        }
+        m_prune_accounts[address] = prune;
+    }
+
+    return;
+}
+
+void xsync_prune_t::del(const std::string address, const std::set<enum_height_type> types) {
+    std::unique_lock<std::mutex> lock(m_lock);
+    auto prune_account = m_prune_accounts.find(address);
+    if (prune_account == m_prune_accounts.end()) {
         return;
+    }
+
+    for (auto type:types) {
+        if (prune_account->second.find((enum_height_type)type) != prune_account->second.end()) {
+            prune_account->second.erase((enum_height_type)type);
+        }
     }
 
     m_min_height_accounts[address] = 0;
 
-    std::map<enum_height_type, uint64_t> prune;
-    for (int i = enum_height_type::start; i < enum_height_type::end; i++) {
-        prune[(enum_height_type)i] = 0;
+    if (prune_account->second.empty()){
+        m_min_height_accounts.erase(address);
+        m_prune_accounts[address].clear();
+        m_prune_accounts.erase(address);
     }
-
-    m_prune_accounts[address] = prune;
+    return;
 }
 
-void xsync_prune_t::del(const std::string address) {
+bool xsync_prune_t::empty(const std::string address) {
     std::unique_lock<std::mutex> lock(m_lock);
-    auto prune_account = m_min_height_accounts.find(address);
-    if (prune_account == m_min_height_accounts.end()) {
-        return;
+    auto prune_account = m_prune_accounts.find(address);
+    if (prune_account == m_prune_accounts.end()) {
+        return true;
     }
 
-    m_min_height_accounts.erase(address);
-    m_prune_accounts[address].clear();
-    m_prune_accounts.erase(address);
+    return false;
 }
 
 bool xsync_prune_t::update(const std::string address, const enum_height_type height_type, const uint64_t height) {
     std::unique_lock<std::mutex> lock(m_lock);
-    if (m_min_height_accounts.find(address) == m_min_height_accounts.end()) {
+    auto prune_account = m_prune_accounts.find(address);
+    if (prune_account == m_prune_accounts.end()) {
         return false;
     }
 
-    if (m_prune_accounts[address][height_type] >= height) {
+    if (prune_account->second.find(height_type) == prune_account->second.end()) {
         return true;
     }
 
-    m_prune_accounts[address][height_type] = height;
+    if (prune_account->second[height_type] >= height) {
+        return true;
+    }
 
-    auto prune_account = m_prune_accounts[address];
+    prune_account->second[height_type] = height;
     uint64_t min = (uint64_t)-1;
-    for (int i = enum_height_type::start; i < enum_height_type::end; i++) {
-        if (min > prune_account[(enum_height_type)i]) {
-            min = prune_account[(enum_height_type)i];
-        }
+    for (auto type:prune_account->second){
+       if (min > type.second) {
+            min = type.second;
+        } 
     }
 
     if (m_min_height_accounts[address] < min) {
