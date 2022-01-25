535f900b392e79a20bafe04be453c2919dda5aaa
Hench <hench.ye@topnetwork.org>
2022-01-25 09:37:51 +0800

    fix tx cache ut

--- tests/xtxexecutor/test_tx_cache.cpp
+++ tests/xtxexecutor/test_tx_cache.cpp
@@ -69,7 +69,8 @@ TEST_F(test_tx_cache, test_cache) {
     xtransaction_cache_data_t cache_data;
     EXPECT_EQ(m_transaction_cache->tx_get(tx_hash, cache_data), 1);
     xJson::Value jv;
-    m_transaction_cache->tx_set_json(tx_hash, jv);
+    m_transaction_cache->tx_set_json(tx_hash, 0, jv);
+    m_transaction_cache->tx_set_json(tx_hash, 1, jv);
     m_transaction_cache->tx_erase(tx_hash);
     EXPECT_EQ(m_transaction_cache->tx_find(tx_hash), 0);
     EXPECT_EQ(m_transaction_cache->tx_find(tx_hash2), 1);
