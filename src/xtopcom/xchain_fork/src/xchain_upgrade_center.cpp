0e3acc875d9a3901aa5265eba2d532406eaa0a1c
PaytonWu <payton.wu@outlook.com>
2022-03-17 10:44:03 +0800

    fix UT failures

--- src/xtopcom/xchain_fork/src/xchain_upgrade_center.cpp
+++ src/xtopcom/xchain_fork/src/xchain_upgrade_center.cpp
@@ -62,7 +62,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 7126740, "v3 block fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 7221960, "tx v2 fee fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 7473960, "partly remove confirm"},
-            xfork_point_t{xfork_point_type_t::logic_time, 7472520, "election contract store credit score"},
+            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "election contract store credit score"},
         };
 
         // !!!change!!! fork time for galileo
@@ -73,7 +73,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 7126740, "v3 block fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 7221960, "tx v2 fee fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 7473960, "partly remove confirm"},
-            xfork_point_t{xfork_point_type_t::logic_time, 7472520, "election contract store credit score"},
+            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "election contract store credit score"},
         };
 
         // !!!change!!! fork time for local develop net
@@ -84,7 +84,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 7126740, "v3 block fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 7221960, "tx v2 fee fork point"},//2022-2-21 10:00:00
             xfork_point_t{xfork_point_type_t::logic_time, 7473960, "partly remove confirm"},
-            xfork_point_t{xfork_point_type_t::logic_time, 7472520, "election contract store credit score"},
+            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "election contract store credit score"},
         };
 #endif  // #if defined(XCHAIN_FORKED_BY_DEFAULT)
         xchain_fork_config_t const & xtop_chain_fork_config_center::chain_fork_config() noexcept {
