3b4e8f1feb66101ca73edfa651ce701c1fd3fbc9
CharlesLiu <charles.liu@upblocks.io>
2022-03-30 11:00:53 +0800

    add xevm

---
+++ src/xtopcom/xevm/xerror/xerror.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2017-2021 Telos Foundation & contributors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+#include "xbase/xns_macro.h"
+
+#include <stdexcept>
+#include <string>
+#include <system_error>
+
+NS_BEG3(top, evm, error)
+
+enum class xenum_errc {
+    ok = 0,
+
+    unknown_error,
+};
+using xerrc_t = xenum_errc;
+
+std::error_code make_error_code(xerrc_t const ec) noexcept;
+std::error_condition make_error_condition(xerrc_t const ec) noexcept;
+
+std::error_category const & evm_category();
+
+NS_END3
+
+NS_BEG1(std)
+
+template <>
+struct is_error_code_enum<top::evm::error::xerrc_t> : std::true_type {};
+
+template <>
+struct is_error_condition_enum<top::evm::error::xerrc_t> : std::true_type {};
+
+NS_END1
