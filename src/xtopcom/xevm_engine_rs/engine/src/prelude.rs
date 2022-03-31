b3694c90816ef4431538b116d3102e2baa9bdc52
CharlesLiu <charles.liu@upblocks.io>
2022-03-31 13:49:22 +0800

    delete unused AccountId. use Address(H160) directly

--- src/xtopcom/xevm_engine_rs/engine/src/prelude.rs
+++ src/xtopcom/xevm_engine_rs/engine/src/prelude.rs
@@ -4,7 +4,6 @@ mod v0 {
     pub use engine_sdk as sdk;
     pub use engine_sdk::types::*;
     pub use engine_transactions as transactions;
-    pub use engine_types::account_id::*;
     pub use engine_types::storage::*;
     pub use engine_types::types::*;
     pub use engine_types::*;
