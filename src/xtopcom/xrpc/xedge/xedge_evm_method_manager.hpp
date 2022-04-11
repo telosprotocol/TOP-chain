2e98767adf671f0008984b887ea39b18c9e650d7
“Guanges” <741131987@qq.com>
2022-04-11 20:06:50 +0800

    add V3 transaction

--- src/xtopcom/xrpc/xedge/xedge_evm_method_manager.hpp
+++ src/xtopcom/xrpc/xedge/xedge_evm_method_manager.hpp
@@ -142,7 +142,7 @@ void xedge_evm_method_base<T>::do_method(shared_ptr<conn_type> & response, xjson
     if (jsonrpc_version == "2.0") {
         xinfo_rpc("rpc request eth");
         std::string method = json_proc.m_request_json["method"].asString();
-        if (method != "eth_sendRawTransaction") {
+        if (method != "eth_sendRawTransaction" && method != "eth_getTransactionByHash") {
             xJson::Value eth_res;
             dev::eth::ClientBase client;
             dev::rpc::Eth eth(client);
