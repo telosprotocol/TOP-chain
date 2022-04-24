81ea66e41a2ede7810d882439a6ca29c9dab20aa
nathan <nathan.sun@topnetwork.org>
2022-04-24 17:09:23 +0800

    fix comments

--- src/xtopcom/xdata/xbstate_ctx.h
+++ src/xtopcom/xdata/xbstate_ctx.h
@@ -83,7 +83,7 @@ class xbstate_ctx_t {
     evm_common::u256    tep_token_balance(const std::string & prop, const std::string& token_name);
     int32_t     tep_token_deposit(const std::string & prop, const std::string& token_name, evm_common::u256 add_token);
     int32_t     tep_token_withdraw(const std::string & prop, const std::string& token_name, evm_common::u256 sub_token);
-    bool        set_tep_balance(const std::string & prop, const std::string & token_name, evm_common::u256 new_balance);
+    int32_t     set_tep_balance(const std::string & prop, const std::string & token_name, evm_common::u256 new_balance);
 
  private:
     int32_t                                 check_create_property(const std::string& key);
