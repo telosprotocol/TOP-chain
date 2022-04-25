75846a75308b6a84686339e6d2d00262cb58e830
ixiaolonglong <956599264@qq.com>
2022-04-25 00:43:26 -0700

    adapt reward old and new struct

--- src/xtopcom/xrpc/xrpc_query_manager.cpp
+++ src/xtopcom/xrpc/xrpc_query_manager.cpp
@@ -287,7 +287,7 @@ void xrpc_query_manager::getIssuanceDetail(xJson::Value & js_req, xJson::Value &
               data::system_contract::XPROPERTY_REWARD_DETAIL);
         return;
     }
-    data::system_contract::xissue_detail issue_detail;
+    data::system_contract::xissue_detail_v2 issue_detail;
     if (issue_detail.from_string(xissue_detail_str) <= 0) {
         xwarn("[grpc::getIssuanceDetail] deserialize failed");
     }
@@ -317,10 +317,12 @@ void xrpc_query_manager::getIssuanceDetail(xJson::Value & js_req, xJson::Value &
     jv["archive_reward_ratio"] = issue_detail.m_archive_reward_ratio;
     jv["validator_reward_ratio"] = issue_detail.m_validator_reward_ratio;
     jv["auditor_reward_ratio"] = issue_detail.m_auditor_reward_ratio;
+    jv["eth_reward_ratio"] = issue_detail.m_eth_reward_ratio;
     jv["vote_reward_ratio"] = issue_detail.m_vote_reward_ratio;
     jv["governance_reward_ratio"] = issue_detail.m_governance_reward_ratio;
     jv["validator_group_count"] = (xJson::UInt)issue_detail.m_validator_group_count;
     jv["auditor_group_count"] = (xJson::UInt)issue_detail.m_auditor_group_count;
+    jv["eth_group_count"] = (xJson::UInt)issue_detail.m_eth_group_count;
     std::map<std::string, std::string> contract_auditor_votes;
     if (m_store->get_map_property(
             sys_contract_zec_vote_addr, issue_detail.m_zec_vote_contract_height, data::system_contract::XPORPERTY_CONTRACT_TICKETS_KEY, contract_auditor_votes) != 0) {
