4aacb69c6b269772954ee03922eb9e062151ed59
“Guanges” <741131987@qq.com>
2022-04-27 11:21:49 +0800

    recover standby rpc return single node and fix code review issue

--- src/xtopcom/xdata/xserial_transfrom.h
+++ src/xtopcom/xdata/xserial_transfrom.h
@@ -1,33 +1,9 @@
 #pragma once
 
-#include <stdio.h>
-#include <iostream>
 #include <string>
-#include "xevm_common/rlp.h"
-#include "xevm_common/address.h"
+#include "xbase/xrefcount.h"
 #include "xevm_common/common.h"
 #include "xevm_common/fixed_hash.h"
-#include "json/json.h"
-#include "xbase/xmem.h"
-#include "xbase/xcontext.h"
-#include "xbase/xrefcount.h"
-#include "trezor-crypto/sha3.h"
-#include "xcrypto/xckey.h"
-#include "xutility/xhash.h"
-#include "xbase/xutl.h"
-#include <secp256k1/secp256k1.h>
-#include <secp256k1/secp256k1_recovery.h>
-
-using h520 = top::evm_common::FixedHash<65>;
-using Signature = h520;
-using h512 = top::evm_common::FixedHash<64>;
-using Public = h512;
-
-enum Type {
-    NullTransaction,   ///< Null transaction.
-    ContractCreation,  ///< Transaction to create contracts - receiveAddress() is ignored.
-    MessageCall        ///< Transaction to invoke a message call - receiveAddress() is used.
-};
 
 namespace top {
 namespace data {
