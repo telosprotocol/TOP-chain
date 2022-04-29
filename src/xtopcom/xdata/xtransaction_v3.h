b1c22d2060810e8c93a52405a653df25996bdcda
“Guanges” <741131987@qq.com>
2022-04-29 09:45:49 +0800

    fix a bug for top-rpc query v3 contract call tx

--- src/xtopcom/xdata/xtransaction_v3.h
+++ src/xtopcom/xdata/xtransaction_v3.h
@@ -96,11 +96,11 @@ class xtransaction_v3_t : public xbase_dataunit_t<xtransaction_v3_t, xdata_type_
     void set_action_type();
     virtual const std::string & get_source_action_name() const override { if (m_eip_xxxx_tx) { return m_eip_xxxx_tx->get_data(); } return strNull; }
     virtual const std::string & get_source_action_para() const override { if (m_eip_xxxx_tx) { return m_eip_xxxx_tx->get_data(); } return strNull; }
-    virtual enum_xaction_type get_source_action_type() const {return xaction_type_asset_out;}
+    virtual enum_xaction_type get_source_action_type() const {return xaction_type_max;}
     virtual std::string get_source_action_str() const;
     virtual const std::string & get_target_action_name() const override { if (m_eip_xxxx_tx) { return m_eip_xxxx_tx->get_data(); } return strNull; }
     virtual const std::string & get_target_action_para() const override { if (m_eip_xxxx_tx) { return m_eip_xxxx_tx->get_data(); } return strNull; }
-    virtual enum_xaction_type get_target_action_type() const {return xaction_type_asset_out;}
+    virtual enum_xaction_type get_target_action_type() const {return xaction_type_max;}
     virtual std::string get_target_action_str() const;
     virtual const std::string & get_authorization() const override {return m_authorization;}
     virtual void                parse_to_json(xJson::Value& tx_json, const std::string & version = RPC_VERSION_V2) const override;
