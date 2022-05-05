dd2a12067b5d9d72a913525d5df6ec74baf6f878
Hench <hench.ye@topnetwork.org>
2022-05-05 14:00:42 +0800

    fix eth_getBalance

--- src/xtopcom/xrpc/eth_rpc/eth_method.h
+++ src/xtopcom/xrpc/eth_rpc/eth_method.h
@@ -82,19 +82,7 @@ public:
     inline void eth_estimateGas(const Json::Value & request, Json::Value & response) {
         response = "0x5208";
     }
-
-    inline void web3_sha3(const Json::Value & request, Json::Value & response) {
-        std::string input = request[0].asString();
-        if (input.size() < 2) {
-            response = "0x";
-            return;
-        }
-        input = top::HexDecode(input.substr(2));
-        const top::uint256_t hash_value = top::utl::xkeccak256_t::digest(input.data(), input.size());
-        std::string hash_str = std::string((char*)hash_value.data(), (std::string::size_type)hash_value.size());
-        response = std::string("0x") + top::HexEncode(hash_str);
-        xdbg("web3_sha3: %s", response.asString().c_str());
-    }
+    void web3_sha3(const Json::Value & request, Json::Value & response);
 };
 
 }  // namespace rpc
