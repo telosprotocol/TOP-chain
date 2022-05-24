8fbdf6599ed54f3de271a69842d30692c1aa98d2
ixiaolonglong <956599264@qq.com>
2022-05-24 00:46:07 -0700

    fix with evm bridge contract and data struct

--- src/xtopcom/xevm_common/xeth/xeth_header.h
+++ src/xtopcom/xevm_common/xeth/xeth_header.h
@@ -10,8 +10,10 @@ NS_BEG3(top, evm_common, eth)
 
 // The log bloom's size (2048-bit).
 using namespace top::evm_common;
+using Hash = h256;
 using Address = h160;
 using LogBloom = h2048;
+using BlockNonce = h64;
 class xeth_block_header_t {
 #define convertStringFromJson(jsonValue, type, address) \
     if ((jsonValue).empty()) {\
@@ -19,62 +21,80 @@ class xeth_block_header_t {
     }\
     (address) = (type)((jsonValue).asString());
 
-#define convertInt64FromJson(jsonValue, address) \
+#define convertUint64FromJson(jsonValue, address) \
     if ((jsonValue).empty()) {\
         return false;\
     }\
-    (address) = strtoll((jsonValue).asString().c_str(), 0, 0);
+    (address) = strtoull((jsonValue).asString().c_str(), 0, 0);
 
 public:
     xeth_block_header_t(){}
     bool fromJson(const std::string& content);
+
+    // encode and decode
     std::string to_string();
-    int32_t from_string();
-    h256 parentHash();
-    h256 uncle_hash();
-    Address miner();
-    h256 stateMerkleRoot();
-    h256 txMerkleRoot();
-    h256 receiptMerkleRoot();
-    LogBloom logBloom();
-    bigint difficulty();
-    int64_t number();
-    u256 gasLimit();
-    u256 gasUsed();
-    int64_t time();
-    std::vector<uint8_t> extra();
-    h256 mixDigest();
-    h64 nonce();
-    bigint baseFee();
-    h256 hash();
+    int from_string(const std::string & s);
+
+    // member of header
+    Hash parentHash() const;
+    Hash uncle_hash() const;
+    Address miner() const;
+    Hash stateMerkleRoot() const;
+    Hash txMerkleRoot() const;
+    Hash receiptMerkleRoot() const;
+    LogBloom logBloom() const;
+    bigint difficulty() const;
+    bigint number() const;
+    uint64_t gasLimit() const;
+    uint64_t gasUsed() const;
+    uint64_t time() const;
+    bytes extra() const;
+    Hash mixDigest() const;
+    BlockNonce nonce() const;
+    bigint baseFee() const;
+    Hash hash();
     bool isBaseFee();
-    h256 hashWithoutSeal();
+    Hash hashWithoutSeal();
 
 private:
     bytes encode_rlp();
     bytes encode_rlp_withoutseal();
 private:
-    h256    m_parentHash;
-    h256    m_uncleHash;
-    Address   m_miner;
-    h256    m_stateMerkleRoot;
-    h256    m_txMerkleRoot;
-    h256    m_receiptMerkleRoot;
-    LogBloom  m_bloom;
-    bigint    m_difficulty;
-    int64_t    m_number;
-    u256    m_gasLimit;
-    u256    m_gasUsed;
-    int64_t  m_time;
-    std::vector<uint8_t>  m_extra;
-    h256    m_mixDigest;
-    h64   m_nonce;
+    Hash m_parentHash;
+    Hash m_uncleHash;
+    Address m_miner;
+    Hash m_stateMerkleRoot;
+    Hash m_txMerkleRoot;
+    Hash m_receiptMerkleRoot;
+    LogBloom m_bloom;
+    bigint m_difficulty;
+    bigint m_number;
+    uint64_t m_gasLimit;
+    uint64_t m_gasUsed;
+    uint64_t m_time;
+    bytes m_extra;
+    Hash m_mixDigest;
+    BlockNonce m_nonce;
 
-	// BaseFee was added by EIP-1559 and is ignored in legacy headers.
-	bigint    m_baseFee; //BaseFee *big.Int `json:"baseFeePerGas" rlp:"optional"`
-    h256    m_hash;
-    bool    m_hashed = false;
-    bool    m_isBaseFee = false;
+    // BaseFee was added by EIP-1559 and is ignored in legacy headers.
+    bigint m_baseFee;
+
+    // Peculiar data
+    Hash m_hash;
+    bool m_hashed = false;
+    bool m_isBaseFee = false;
+};
+
+class xeth_block_header_with_difficulty_t {
+public:
+    xeth_block_header_with_difficulty_t() = default;
+    xeth_block_header_with_difficulty_t(xeth_block_header_t header, bigint difficulty);
+
+    std::string to_string();
+    int from_string(const std::string & s);
+
+    xeth_block_header_t m_header;
+    bigint m_difficult_sum;
 };
 
 NS_END3
