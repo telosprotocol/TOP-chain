c2ad8ed73b5b69406d7aac29a87a1721befac3c1
Charles.Liu <charles.liu@upblocks.io>
2022-06-15 17:55:52 +0800

    add wasmer ffi && ut

--- src/xtopcom/xwasmer_engine/src/interface.rs
+++ src/xtopcom/xwasmer_engine/src/interface.rs
@@ -12,7 +12,15 @@ const DEFAULT_INSTANCE_OPTIONS: InstanceOptions = InstanceOptions {
 pub fn vm_res_handle(vm_res: VmResult<Val>) -> i32 {
     match vm_res {
         Ok(res) => {
-            println!("rustvm execute ok {}", res.unwrap_i32());
+            match res.ty() {
+                wasmer::ValType::I32 => println!("rustvm execute ok {}", res.unwrap_i32()),
+                wasmer::ValType::I64 => println!("rustvm execute ok {}", res.unwrap_i64()),
+                wasmer::ValType::F32 => println!("rustvm execute ok {}", res.unwrap_f32()),
+                wasmer::ValType::F64 => println!("rustvm execute ok {}", res.unwrap_f64()),
+                wasmer::ValType::V128 => println!("rustvm execute ok {}", res.unwrap_v128()),
+                _ => (),
+            }
+
             0
         }
         Err(err) => match err {
@@ -29,47 +37,30 @@ pub fn vm_res_handle(vm_res: VmResult<Val>) -> i32 {
 }
 
 #[no_mangle]
-pub extern "C" fn get_erc20_instance(s: *const u8, size: u32) -> Box<Instance> {
-    let ptr = s;
+pub extern "C" fn deploy_contract(code_ptr: *const u8, size: u32) -> Box<Instance> {
     let mut wasm = Vec::<u8>::new();
 
     for i in 0..size as usize {
         unsafe {
-            let iter = ((ptr as usize) + i) as *const u8;
+            let iter = ((code_ptr as usize) + i) as *const u8;
             wasm.push(*iter);
         }
     }
-    // println!("wasm: {:?}", wasm);
-
     Box::new(Instance::from_code(&wasm, DEFAULT_INSTANCE_OPTIONS, None).unwrap())
 }
 
 #[no_mangle]
-pub extern "C" fn depoly_erc20(
-    ins: &Instance,
-    // symbol: *const c_char,
-    // total_allowance: u64,
-    params: i64,
-) -> i32 {
-    let gas: i32 = 0;
-    vm_res_handle(ins.call_function1(
-        "depoly",
-        &[Val::I64(params), Val::I64(&gas as *const i32 as i64)],
-    ))
+pub extern "C" fn call_contract_1(ins: &Instance, a: i32, b: i32) -> i32 {
+    vm_res_handle(ins.call_function1("add", &[Val::I32(a), Val::I32(b)]))
 }
 
 #[no_mangle]
-pub extern "C" fn call_erc20(
-    ins: &Instance,
-    // function_name:*const c_char,
-    // function_args:i32,
-    params: i64,
-) -> i32 {
-    let gas: i32 = 0;
-    vm_res_handle(ins.call_function1(
-        "call",
-        &[Val::I64(params), Val::I64(&gas as *const i32 as i64)],
-    ))
+pub extern "C" fn call_contract_2(ins: &Instance) -> i32 {
+    vm_res_handle(ins.call_function1("blk_index", &[]))
+}
+#[no_mangle]
+pub extern "C" fn call_contract_3(ins: &Instance) -> i32 {
+    vm_res_handle(ins.call_function1("nothing", &[]))
 }
 
 #[no_mangle]
