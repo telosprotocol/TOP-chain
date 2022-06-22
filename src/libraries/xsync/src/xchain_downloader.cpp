f26cde78ba5a275704237b9425ef9ae8bff658a7
Hench <hench.ye@topnetwork.org>
2022-06-22 13:51:30 +0800

    fix relay sync

--- src/libraries/xsync/src/xchain_downloader.cpp
+++ src/libraries/xsync/src/xchain_downloader.cpp
@@ -582,6 +582,7 @@ xsync_command_execute_result xchain_downloader_t::execute_download(uint64_t star
                 return abort;
             }
         } else if (account_prefix == sys_contract_eth_table_block_addr) {
+        } else if (account_prefix == sys_contract_relay_table_block_base_addr) {
         } else {
             xsync_dbg("xchain_downloader_t::execute_download check fail: %s", m_address.c_str());
             return abort;
