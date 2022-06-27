8c4a5b335ffb93621e5889ef410dab95752514ac
payton.wu <payton.wu@outlook.com>
2022-06-27 17:59:06 +0800

    add UT for delegate ERC20.

---
+++ tests/xevm_engine_test/test_cases/delegate_erc20/staticcall_erc20.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+contract TopErc20Wrapper {
+    address constant TopErc20 = 0xff00000000000000000000000000000000000001;
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    bytes1 public uuid;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, bytes1 uuid_) {
+        name = name_;
+        symbol = symbol_;
+        decimals = decimals_;
+        uuid = uuid_;
+    }
+
+    fallback() external {
+        bytes memory call_data = abi.encodePacked(uuid, msg.data);
+        (bool success, bytes memory result) = TopErc20.staticcall(call_data);
+
+        require(success, string(result));
+
+        assembly {
+            return(add(result, 0x20), mload(result))
+        }
+    }
+}
