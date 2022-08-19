10577d9b2203366cd85a4caa872fcb2bd9b7a55c
nathan <nathan.sun@topnetwork.org>
2022-08-19 15:54:57 +0800

    unit save to db with table block

--- src/xtopcom/xdata/xentire_block.hpp
+++ src/xtopcom/xdata/xentire_block.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "xbasic/xserialize_face.h"
 #include "xdata/xblock.h"
+#include "xdata/xblocktool.h"
 #include "xdata/xtransaction.h"
 #include "xbase/xobject_ptr.h"
 #include "xmetrics/xmetrics.h"
@@ -29,38 +30,15 @@ public:
                     xerror("xentire_block_t::do_write not table block,but carry_unit_blocks is true:%s", block_ptr->dump().c_str());
                     return -1;
                 }
+                
+                // todo(nathan):获取unit块放到外面。
+                xblocktool_t::get_subblocks(block_ptr, unit_blocks);
 
-                uint32_t num = 0;
-                std::vector<xblock_ptr_t> blocks;
-                auto unit_infos_str = block_ptr->get_unit_infos();
-                xdbg("xentire_block_t::do_write table block:%s, unit_infos:%s", block_ptr->dump().c_str(), unit_infos_str.c_str());
-                if (!unit_infos_str.empty()) {
-                    base::xtable_unit_infos_t unit_infos;
-                    unit_infos.serialize_from_string(unit_infos_str);
-                    auto &unit_infos_vec = unit_infos.get_unit_infos();
-                    xdbg("xentire_block_t::do_write table block:%s, unit_infos_vec size:%u", block_ptr->dump().c_str(), unit_infos_vec.size());
-
-                    for (auto & unit_info : unit_infos_vec) {
-                        base::xvaccount_t account(unit_info.get_addr());
-                        base::xauto_ptr<base::xvblock_t> unit_block = base::xvchain_t::instance().get_xblockstore()->load_block_object(account, unit_info.get_height(), unit_info.get_hash(), true);
-                        if (unit_block != nullptr) {
-                            unit_block->add_ref();
-                            base::xvblock_t *vblock = unit_block.get();
-                            data::xblock_t *block = (data::xblock_t*)vblock;
-                            data::xblock_ptr_t unit_block_ptr = nullptr;
-                            unit_block_ptr.attach(block);
-                            blocks.push_back(unit_block_ptr);
-                        } else {
-                            xwarn("xentire_block_t::do_write table block:%s, unit block load fail account:%s,height:%llu,hash:%s,hash size:%u", block_ptr->dump().c_str(), unit_info.get_addr().c_str(), unit_info.get_height(), xstring_utl::to_hex(unit_info.get_hash()).c_str(), unit_info.get_hash().size());
-                            return -1;
-                        }
-                    }
-                }
-
-                num = blocks.size();
+                uint32_t num = unit_blocks.size();
                 stream << num;
                 xdbg("xentire_block_t::do_write table block:%s, unit num:%d", block_ptr->dump().c_str(), num);
-                for (auto & block :blocks) {
+                for (auto & unit_block :unit_blocks) {
+                    data::xblock_t *block = (data::xblock_t*)(unit_block.get());
                     block->full_block_serialize_to(stream);
                     xdbg("xentire_block_t::do_write succ table block:%s,unit block:%s", block_ptr->dump().c_str(), block->dump().c_str());
                 }
@@ -105,7 +83,7 @@ private:
 
 public:
     xblock_ptr_t block_ptr{};
-    std::vector<xblock_ptr_t> unit_blocks;
+    std::vector<xvblock_ptr_t> unit_blocks;
     bool carry_unit_blocks{false};
 };
 
