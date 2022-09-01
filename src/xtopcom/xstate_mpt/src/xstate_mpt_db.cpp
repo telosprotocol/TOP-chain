db4b0c3d54ed843a76be812ffc7faa395dd5ca17
LonDong <lon.dong@topnetwork.org>
2022-09-01 16:25:44 +0800

    fix hex str and state mpt empty not return error

--- src/xtopcom/xstate_mpt/src/xstate_mpt_db.cpp
+++ src/xtopcom/xstate_mpt/src/xstate_mpt_db.cpp
@@ -17,7 +17,7 @@ void xtop_state_mpt_db::Put(xbytes_t const & key, xbytes_t const & value, std::e
     std::lock_guard<std::mutex> lock(m_mutex);
     if (m_db->set_value({key.begin(), key.end()}, {value.begin(), value.end()}) == false) {
         xwarn("xtop_state_mpt_db::Put key: %s, value: %s, error", top::to_hex(key).c_str(), top::to_hex(value).c_str());
-        ec = error::xerrc_t::mpt_set_error;
+        ec = error::xerrc_t::state_mpt_db_set_error;
         return;
     }
     xdbg("xtop_state_mpt_db::Put key: %s, value: %s", top::to_hex(key).c_str(), top::to_hex(value).c_str());
@@ -29,7 +29,7 @@ void xtop_state_mpt_db::Delete(xbytes_t const & key, std::error_code & ec) {
 
     if (m_db->delete_value({key.begin(), key.end()}) == false) {
         xwarn("xtop_state_mpt_db::Delete key: %s, error", top::to_hex(key).c_str());
-        ec = error::xerrc_t::mpt_delete_error;
+        ec = error::xerrc_t::state_mpt_db_delete_error;
         return;
     }
     xdbg("xtop_state_mpt_db::Delete key: %s", top::to_hex(key).c_str());
@@ -46,7 +46,7 @@ xbytes_t xtop_state_mpt_db::Get(xbytes_t const & key, std::error_code & ec) {
     auto value = m_db->get_value({key.begin(), key.end()});
     if (value == std::string()) {
         xwarn("xtop_state_mpt_db::Get key: %s, not found", top::to_hex(key).c_str());
-        ec = error::xerrc_t::mpt_not_found;
+        ec = error::xerrc_t::state_mpt_db_not_found;
         return {};
     }
     xdbg("xtop_state_mpt_db::Get key: %s, value: %s", top::to_hex(key).c_str(), top::to_hex(value).c_str());
