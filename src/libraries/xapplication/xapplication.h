5820bda51346bbcf54aa44366f90ede9ba3f63ff
Charles.Liu <charles.liu@upblocks.io>
2022-09-29 14:50:56 +0800

    refactor xapplication struct

--- src/libraries/xapplication/xapplication.h
+++ src/libraries/xapplication/xapplication.h
@@ -1,37 +1,62 @@
-// Copyright (c) 2017-2018 Telos Foundation & contributors
+// Copyright (c) 2017-present Telos Foundation & contributors
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #pragma once
 
-#include "xapplication/xapplication_face.h"
-#include "xapplication/xapplication_fwd.h"
-#include "xapplication/xchain_application.h"
+#include "xbase/xns_macro.h"
+#include "xbasic/xasio_io_context_wrapper.h"
 #include "xbasic/xcrypto_key.h"
 #include "xbasic/xmemory.hpp"
-#include "xbasic/xtimer_driver.h"
+#include "xbasic/xrunnable.h"
+#include "xbasic/xtimer_driver_fwd.h"
+#include "xcommon/xnode_id.h"
+
+#include "xblockstore/xsyncvstore_face.h"
 #include "xchain_timer/xchain_timer_face.h"
-#include "xcommon/xip.h"
-#include "xcommon/xlogic_time.h"
-#include "xcommon/xnode_info.h"
-#include "xconfig/xconfig_register.h"
-#include "xsystem_contract_runtime/xsystem_contract_manager.h"
-#include "xdata/xblocktool.h"
-#include "xelect/client/xelect_client.h"
-#include "xelect_net/include/elect_main.h"
-#include "xelect_net/include/elect_vhost_face.h"
-#include "xgenesis/xgenesis_manager.h"
-#include "xmbus/xmessage_bus.h"
-#include "xrouter/xrouter_face.h"
 #include "xdbstore/xstore_face.h"
-#include "xsync/xsync_object.h"
+#include "xgenesis/xgenesis_manager_fwd.h"
+#include "xmbus/xmessage_bus_face.h"
+#include "xrouter/xrouter_face.h"
 #include "xtxpool_v2/xtxpool_face.h"
 #include "xtxstore/xtxstore_face.h"
+#include "xvledger/xvblockstore.h"
+#include "xvledger/xvcertauth.h"
+#include "xvledger/xvcnode.h"
+#include "xvledger/xvtxstore.h"
 
-#include <cstdint>
+#include "xdata/xelection/xelection_result_store.h"
+#include "xelect/client/xelect_client_process.h"
+#include "xelection/xcache/xdata_accessor_face.h"
+#include "xtxpool_service_v2/xtxpool_service_face.h"
+#include "xvnetwork/xmessage_callback_hub.h"
+#include "xvnetwork/xvhost_face_fwd.h"
+#include "xvnode/xvnode_manager_face.h"
+
 #include <memory>
+#include <string>
 #include <unordered_map>
-#include <vector>
+
+/// ==== begin fwd
+
+NS_BEG2(top, grpcmgr)
+class xgrpc_mgr_t;
+NS_END2
+
+NS_BEG2(top, sync)
+class xtop_sync_object;
+using xsync_object_t = xtop_sync_object;
+NS_END2
+
+NS_BEG2(top, elect)
+class xelect_client_imp;
+
+// todo totally remove file elect_main.h
+class MultilayerNetwork;
+using ElectMain = MultilayerNetwork;
+NS_END2
+
+/// ===== end fwd
 
 NS_BEG2(top, application)
 
@@ -41,9 +66,9 @@ using xthread_pool_type_t = xtop_thread_pool_type;
 enum class xtop_io_context_type : uint8_t { invalid, general };
 using xio_context_type_t = xtop_io_context_type;
 
-NS_END2
-
 #if !defined XCXX14_OR_ABOVE
+
+NS_END2
 
 NS_BEG1(std)
 
@@ -60,100 +85,73 @@ struct hash<top::application::xio_context_type_t> final {
 NS_END1
 
 #endif
-
 NS_BEG2(top, application)
 
-class xtop_application final : public xapplication_face_t<xtop_application> {
+class xtop_application final : public xtrival_runnable_t<xtop_application> {
 public:
     using xthread_pool_t = std::vector<xobject_ptr_t<base::xiothread_t>>;
     using xio_context_pool_t = std::vector<std::shared_ptr<xbase_io_context_wrapper_t>>;
 
 private:
-    using xbase_t = xapplication_face_t<xtop_application>;
-
     common::xnode_id_t m_node_id;
     xpublic_key_t m_public_key;
     std::string m_sign_key;
 
-    std::unordered_map<xio_context_type_t, xio_context_pool_t> m_io_context_pools;
+    common::xnetwork_id_t m_network_id;
 
+    std::unordered_map<xio_context_type_t, xio_context_pool_t> m_io_context_pools;
     std::shared_ptr<xbase_timer_driver_t> m_timer_driver;
     std::unique_ptr<elect::ElectMain> m_elect_main;
-
     std::unique_ptr<router::xrouter_face_t> m_router;
     xobject_ptr_t<mbus::xmessage_bus_face_t> m_bus;
-    xobject_ptr_t<store::xstore_face_t> m_store;
-    xobject_ptr_t<base::xvblockstore_t> m_blockstore;
-    xobject_ptr_t<base::xvtxstore_t> m_txstore;
     xobject_ptr_t<time::xchain_time_face_t> m_logic_timer;
     xobject_ptr_t<base::xiothread_t> m_grpc_thread{};
     xobject_ptr_t<base::xiothread_t> m_sync_thread{};
-    std::vector<xobject_ptr_t<base::xiothread_t>> m_sync_account_thread_pool{};
-    std::vector<xobject_ptr_t<base::xiothread_t>> m_sync_handler_thread_pool{};
     std::unique_ptr<elect::xelect_client_imp> m_elect_client;
-    xobject_ptr_t<xtxpool_v2::xtxpool_face_t> m_txpool;
-    std::unordered_map<xthread_pool_type_t, xthread_pool_t> m_thread_pools;
+    xobject_ptr_t<store::xstore_face_t> m_store;
+    xobject_ptr_t<base::xvblockstore_t> m_blockstore;
+    xobject_ptr_t<base::xvtxstore_t> m_txstore;
     xobject_ptr_t<base::xvnodesrv_t> m_nodesvr_ptr;
     xobject_ptr_t<base::xvcertauth_t> m_cert_ptr;
-    xobject_ptr_t<store::xsyncvstore_t> m_syncstore;
-    std::unique_ptr<contract_runtime::system::xsystem_contract_manager_t> m_sys_contract_mgr;
-    std::vector<std::unique_ptr<xchain_application_t>> m_chain_applications{};
     std::unique_ptr<genesis::xgenesis_manager_t> m_genesis_manager;
+
+    xobject_ptr_t<xtxpool_v2::xtxpool_face_t> m_txpool;
+    xobject_ptr_t<store::xsyncvstore_t> m_syncstore;
+    std::unordered_map<xthread_pool_type_t, xthread_pool_t> m_thread_pools;
+    std::vector<xobject_ptr_t<base::xiothread_t>> m_sync_account_thread_pool{};
+    std::vector<xobject_ptr_t<base::xiothread_t>> m_sync_handler_thread_pool{};
+
+    std::shared_ptr<elect::xelect_client_process> m_elect_client_process;  // should be xobject_ptr_t
+    std::unique_ptr<election::cache::xdata_accessor_face_t> m_election_cache_data_accessor;
+    std::shared_ptr<vnetwork::xvhost_face_t> m_vhost;
+    std::shared_ptr<vnetwork::xmessage_callback_hub_t> m_message_callback_hub;
+    std::unique_ptr<sync::xsync_object_t> m_sync_obj;
+    std::unique_ptr<grpcmgr::xgrpc_mgr_t> m_grpc_mgr;
+    std::shared_ptr<xtxpool_service_v2::xtxpool_service_mgr_face> m_txpool_service_mgr;
+    std::shared_ptr<vnode::xvnode_manager_face_t> m_vnode_manager;
 
 public:
     xtop_application(xtop_application const &) = delete;
     xtop_application & operator=(xtop_application const &) = delete;
     xtop_application(xtop_application &&) = default;
     xtop_application & operator=(xtop_application &&) = default;
-    ~xtop_application() override = default;
+    ~xtop_application() = default;
 
     xtop_application(common::xnode_id_t const & node_id, xpublic_key_t const & public_key, std::string const & sign_key);
 
+public:
     void start() override;
 
     void stop() override;
 
-    common::xnode_id_t const & node_id() const noexcept;
-
-    xpublic_key_t const & public_key() const noexcept;
-
-    std::string const & sign_key() const noexcept;
-
-    observer_ptr<xbase_timer_driver_t> timer_driver() const noexcept;
-
-    observer_ptr<elect::xnetwork_driver_face_t> network_driver(common::xnetwork_id_t const & network_id) const noexcept;
-
-    observer_ptr<time::xchain_time_face_t> logic_timer() const noexcept;
-
-    observer_ptr<mbus::xmessage_bus_face_t> message_bus() const noexcept;
-
-    observer_ptr<store::xstore_face_t> store() const noexcept;
-
-    observer_ptr<base::xvblockstore_t> blockstore() const noexcept;
-
-    observer_ptr<base::xvtxstore_t> txstore() const noexcept;
-
-    observer_ptr<router::xrouter_face_t> router() const noexcept;
-
-    observer_ptr<base::xiothread_t> thread(std::size_t const index) const noexcept;
-
-    std::shared_ptr<top::elect::ElectManager> elect_manager() const noexcept;
-
-    observer_ptr<elect::ElectMain> elect_main() const noexcept;
-
-    xthread_pool_t const & thread_pool(xthread_pool_type_t const thread_pool_type) const noexcept;
-
-    observer_ptr<xtxpool_v2::xtxpool_face_t> txpool() const noexcept;
-
-    xobject_ptr_t<base::xvnodesrv_t> node_service() const noexcept;
-
-    xobject_ptr_t<base::xvcertauth_t> cert_serivce() const noexcept;
-
-    xobject_ptr_t<store::xsyncvstore_t> syncstore() const noexcept;
-
-    observer_ptr<contract_runtime::system::xsystem_contract_manager_t> system_contract_manager() const noexcept;
-
+public:
 private:
+    void on_election_data_updated(data::election::xelection_result_store_t const & election_result_store,
+                                  common::xzone_id_t const & zid,
+                                  std::uint64_t const associated_blk_height);
+
+    void load_last_election_data();
+
     base::xauto_ptr<top::base::xvblock_t> last_logic_time() const;
 
     int32_t handle_register_node(std::string const & node_addr, std::string const & node_sign);
@@ -163,7 +161,9 @@ private:
     bool is_genesis_node() const noexcept;
 
     bool is_beacon_account() const noexcept;
+
+    void top_console_init();
 };
-using xtop_application_t = xtop_application;
+using xapplication_t = xtop_application;
 
 NS_END2
---
+++ src/xtopcom/xgenesis/xgenesis_manager_fwd.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2017-2021 Telos Foundation & contributors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+#include "xbase/xns_macro.h"
+
+NS_BEG2(top, genesis)
+
+class xtop_genesis_manager;
+using xgenesis_manager_t = xtop_genesis_manager;
+
+NS_END2
