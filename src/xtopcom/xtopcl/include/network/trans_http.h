eba0f873aed3716b5bba127f0751f2f934fdc0a5
Charles.Liu <charles.liu@upblocks.io>
2022-10-09 10:03:56 +0800

    refactor trans_http's http_client to async_http_base derived

--- src/xtopcom/xtopcl/include/network/trans_http.h
+++ src/xtopcom/xtopcl/include/network/trans_http.h
@@ -1,35 +1,38 @@
 #pragma once
 
-#ifndef XCHAIN_TRANS_HTTP
-#define XCHAIN_TRANS_HTTP
+#include "xhttp/xhttp_client_base.h"
+#include "xtopcl/include/network/trans_base.h"
 
 #include <memory>
 #include <thread>
-#include "xtopcl/include/network/trans_base.h"
-#include "simplewebserver/client_http.hpp"
 
-namespace xChainSDK
-{
-	using HttpClient = SimpleWeb::Client<SimpleWeb::HTTP>;
+namespace xChainSDK {
 
-	class trans_http : public trans_base
-	{
-	public:
-		trans_http(const std::string& host);
-		virtual ~trans_http();
+class trans_http_client : public top::xhttp::xhttp_client_async_base_t {
+private:
+    using base_t = top::xhttp::xhttp_client_async_base_t;
 
-		virtual int do_trans(const std::string& content);
+public:
+    trans_http_client(std::string const & ip_port);
 
-		// void thread_func();
+    void do_request(std::string const & content);
+};
 
-	private:
-		HttpClient client;
-		// std::thread thread_;
-		// std::mutex mutex_;
-		// std::condition_variable cv_;
-		bool shut_down_{ false };
-        // std::atomic_int notify_count_{ 0 };
-	};
-}
+class trans_http : public trans_base {
+public:
+    trans_http(const std::string & host);
+    virtual ~trans_http();
 
-#endif // !XCHAIN_TRANS_HTTP
+    virtual int do_trans(const std::string & content);
+
+    // void thread_func();
+
+private:
+    trans_http_client async_client;
+    // std::thread thread_;
+    // std::mutex mutex_;
+    // std::condition_variable cv_;
+    bool shut_down_{false};
+    // std::atomic_int notify_count_{ 0 };
+};
+}  // namespace xChainSDK
