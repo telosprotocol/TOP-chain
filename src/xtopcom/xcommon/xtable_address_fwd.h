af29dd7927db73ce1b4e2d2e3992dc70cf71498c
payton.wu <payton.wu@outlook.com>
2022-11-02 16:27:02 +0800

    db_export

---
+++ src/xtopcom/xcommon/xtable_address_fwd.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2017-present Telos Foundation & contributors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+#include "xcommon/xaccount_address_fwd.h"
+
+NS_BEG2(top, common)
+
+using xtable_address_t = common::xaccount_address_t;
+
+NS_END2
