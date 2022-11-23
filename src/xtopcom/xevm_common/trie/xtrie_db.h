471ae7b0fd2922f60fa9f0704e3f98a6778c2a94
LonDong <lon.dong@topnetwork.org>
2022-11-23 04:35:10 -0500

    add MPT optimize UT

--- src/xtopcom/xevm_common/trie/xtrie_db.h
+++ src/xtopcom/xevm_common/trie/xtrie_db.h
@@ -37,8 +37,7 @@ private:
     friend class xtop_trie_cache_node;
     xkv_db_face_ptr_t diskdb_;  // Persistent storage for matured trie nodes
 
-    // std::map<xh256_t, xbytes_t> cleans_;
-    basic::xlru_cache_specialize<xh256_t, xbytes_t> cleans_{2000}; // TODO(jimmy) 10000
+    basic::xlru_cache_specialize<xh256_t, xbytes_t> cleans_{10000};
     std::map<xh256_t, xtrie_cache_node_t> dirties_;
     std::unordered_set<xh256_t> pruned_hashes_;
 
