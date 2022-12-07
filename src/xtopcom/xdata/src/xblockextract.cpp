df662a48d12206670e589ee6047506b179bd5ecc
jimmy.xu <xzmjack1984@163.com>
2022-12-07 15:51:38 +0800

    optimize actions unpack

--- src/xtopcom/xdata/src/xblockextract.cpp
+++ src/xtopcom/xdata/src/xblockextract.cpp
@@ -17,10 +17,90 @@
 
 NS_BEG2(top, data)
 
+void xblockextract_t::extract_sub_txs(base::xvblock_t* _block, std::vector<base::xvtxindex_ptr> & sub_txs) {
+    if (_block->get_block_class() == base::enum_xvblock_class_nil) {
+        return;
+    }
+
+    uint32_t count = 0;
+    auto & all_entitys = _block->get_input()->get_entitys();
+    for (auto & entity : all_entitys) {
+        // it must be xinentitys
+        base::xvinentity_t* _inentity = dynamic_cast<base::xvinentity_t*>(entity);
+        assert(_inentity != nullptr);
+        auto & all_actions = _inentity->get_actions();
+        for (auto & action : all_actions) {
+            if (action.get_org_tx_hash().empty()) {
+                continue;
+            }
+            base::xvtxindex_ptr tx_index = make_object_ptr<base::xvtxindex_t>(*_block, action.get_org_tx_hash(), (base::enum_transaction_subtype)action.get_org_tx_action_id());
+            sub_txs.push_back(tx_index);
+        }
+    }
+
+    base::xvinentity_t* primary_input_entity = _block->get_input()->get_primary_entity();
+    xassert(primary_input_entity != nullptr);
+    if (!primary_input_entity->get_extend_data().empty()) {
+        xtable_primary_inentity_extend_t _extend;
+        int32_t ret = _extend.serialize_from_string(primary_input_entity->get_extend_data());
+        if (ret <= 0) {
+            xassert(false);
+            return;
+        }
+        base::xvactions_t _ethreceipt_actions = _extend.get_txactions();
+        for (auto & action : _ethreceipt_actions.get_actions()) {
+            if (action.get_org_tx_hash().empty()) {
+                xassert(false);
+                continue;
+            }
+            base::xvtxindex_ptr tx_index = make_object_ptr<base::xvtxindex_t>(*_block, action.get_org_tx_hash(), (base::enum_transaction_subtype)action.get_org_tx_action_id());
+            sub_txs.push_back(tx_index);
+        }
+    }
+
+    return;
+}
+
 uint32_t xblockextract_t::get_txactions_count(base::xvblock_t* _block) {
-    // TODO(jimmy) optimize
-    std::vector<xlightunit_action_t> txactions = unpack_txactions(_block);
-    return (uint32_t)txactions.size();
+    if (_block->get_block_class() == base::enum_xvblock_class_nil) {
+        return 0;
+    }
+
+    uint32_t count = 0;
+    auto & all_entitys = _block->get_input()->get_entitys();
+    for (auto & entity : all_entitys) {
+        // it must be xinentitys
+        base::xvinentity_t* _inentity = dynamic_cast<base::xvinentity_t*>(entity);
+        assert(_inentity != nullptr);
+        auto & all_actions = _inentity->get_actions();
+        for (auto & action : all_actions) {
+            if (action.get_org_tx_hash().empty()) {
+                continue;
+            }
+            count++;
+        }
+    }
+
+    base::xvinentity_t* primary_input_entity = _block->get_input()->get_primary_entity();
+    xassert(primary_input_entity != nullptr);
+    if (!primary_input_entity->get_extend_data().empty()) {
+        xtable_primary_inentity_extend_t _extend;
+        int32_t ret = _extend.serialize_from_string(primary_input_entity->get_extend_data());
+        if (ret <= 0) {
+            xassert(false);
+            return count;
+        }
+        base::xvactions_t _ethreceipt_actions = _extend.get_txactions();
+        for (auto & action : _ethreceipt_actions.get_actions()) {
+            if (action.get_org_tx_hash().empty()) {
+                xassert(false);
+                continue;
+            }
+            count++;
+        }
+    }
+
+    return count;
 }
 
 std::vector<xlightunit_action_t> xblockextract_t::unpack_txactions(base::xvblock_t* _block) {
