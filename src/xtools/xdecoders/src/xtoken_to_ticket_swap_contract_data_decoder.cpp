e04747ae5d6be6a7ed3ca9c129c4f8930ecd8812
payton.wu <payton.wu@outlook.com>
2023-01-03 15:23:52 +0800

    add ticket / token swap contract data decoders & vote data decoder.

---
+++ src/xtools/xdecoders/src/xtoken_to_ticket_swap_contract_data_decoder.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2022-present Telos Foundation & contributors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "../xtoken_to_ticket_swap_contract_data_decoder.h"
+
+#include "xbase/xcontext.h"
+#include "xbase/xmem.h"
+#include "xbasic/xhex.h"
+#include "xbasic/xutility.h"
+
+#include <json/json.hpp>
+
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+
+NS_BEG4(top, tools, decoders, details)
+
+void xtop_token_to_ticket_swap_contract_data_decoder::decode(std::string const & input) {
+    assert(!input.empty());
+
+    auto bytes = from_hex(input);
+    base::xstream_t stream{base::xcontext_t::instance(), bytes.data(), static_cast<uint32_t>(bytes.size())};
+    uint64_t vote_amount;
+    uint16_t lock_duration;
+    stream >> vote_amount;
+    stream >> lock_duration;
+
+    nlohmann::json json;
+    json["vote_num"] = vote_amount;
+    json["duration"] = lock_duration;
+
+    std::cout << json.dump() << std::endl;
+}
+
+NS_END4
---
+++ src/xtools/xdecoders/src/xvote_data_decoder.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2022-present Telos Foundation & contributors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "../xvote_data_decoder.h"
+
+#include "xbase/xcontext.h"
+#include "xbase/xmem.h"
+#include "xbasic/xhex.h"
+#include "xbasic/xutility.h"
+
+#include <json/json.hpp>
+
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+#include <map>
+
+NS_BEG4(top, tools, decoders, details)
+
+void xtop_vote_data_decoder::decode(std::string const & input) {
+    assert(!input.empty());
+
+    auto bytes = from_hex(input);
+    base::xstream_t stream{base::xcontext_t::instance(), bytes.data(), static_cast<uint32_t>(bytes.size())};
+    std::map<std::string, uint64_t> vote_details;
+    stream >> vote_details;
+
+    nlohmann::json json;
+    for (auto const & vote_datum : vote_details) {
+        json[top::get<std::string const>(vote_datum)] = top::get<uint64_t>(vote_datum);
+    }
+
+    std::cout << json.dump() << std::endl;
+}
+
+NS_END4
