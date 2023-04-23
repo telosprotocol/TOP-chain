50ae45deae6c0cda29b2e66ee3d65f3f8b785f9d
jimmy.xu <xzmjack1984@163.com>
2023-04-23 09:05:35 +0000

    fix log clear issue

---
+++ src/xtopcom/xbasic/xsys_utl.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2017-2021 Telos Foundation & contributors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+#include <string>
+#include <set>
+#include "xbase/xns_macro.h"
+
+NS_BEG1(top)
+
+class xsys_utl_t {
+ public:
+    static int    get_log_directory_available_space_GB(std::string const & log_path);
+    static int    check_and_remove_old_log_file(std::string const & full_log_file_path, int threshold_timestamp_s);
+    static int    check_and_remove_old_log_files(std::string const & log_path, int log_keep_seconds, int metric_keep_seconds);
+};
+
+
+NS_END1
